d1144e31c7d13243ead255e823f2cad72f870f3b
jgatzemeier <jgatzemeier@email.arizona.edu>
2018-08-14 14:19:12 -0700

    updated individual makefiles to remove the src files that are in common. Updated header.mk to find the new common library and new common include path.

--- make/header.mk
+++ make/header.mk
@@ -3,8 +3,8 @@ LIB =
 OBJ = 
 
 CFLAGS += -Wall -DPRINT_LOG -no-pie
-INCLUDES = -I. -I$(BUILD_ROOT)/include/libj2735
-LIBS = -L$(BUILD_ROOT)/lib/
+INCLUDES = -I. -I$(BUILD_ROOT)/include/libj2735 -I$(BUILD_ROOT)/include/common
+LIBS = -L$(BUILD_ROOT)/lib/ -L$(BUILD_ROOT)/lib/
 
 .PHONY: all linux clean
 
@@ -15,4 +15,4 @@ linux: AR=ar
 linux: RANLIB=ranlib
 linux: DEVICE=linux
 linux: LIBS+=-lj2735-linux
-
+linux: LIBS+=-lmmitss-common
