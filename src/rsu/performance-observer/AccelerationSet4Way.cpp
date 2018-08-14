25eccd0693e5490f5ca25e44a500e4d5417c65ea
pearson10m <pearson10m@yahoo.com>
2018-08-13 19:25:07 -0700

    Added performance observer files and changed so that it builds

---
+++ src/rsu/performance-observer/AccelerationSet4Way.cpp
@@ -0,0 +1,12 @@
+#include "AccelerationSet4Way.h"
+
+
+
+AccelerationSet4Way::AccelerationSet4Way(void)
+{
+}
+
+
+AccelerationSet4Way::~AccelerationSet4Way(void)
+{
+}
---
+++ src/rsu/performance-observer/PositionLocal3D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class PositionLocal3D
+{
+public:
+	PositionLocal3D(void);
+	~PositionLocal3D(void);
+
+	public:
+	double latitude ; // 4 bytes measured in degrees
+	double longitude ; // 4 bytes measured in degrees
+	double elevation ;  // 2 bytes measured in meters
+	double positionAccuracy ; //4 bytes measured in ?? is this J2735 position accurracy??
+
+	void Init();
+};
+
