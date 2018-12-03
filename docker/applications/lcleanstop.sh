eb21cc983e0cc03db02e9a9582aa1c9cb6a9e4da
Niraj Vasant Altekar <nvaltekar@email.arizona.edu>
2018-12-03 12:22:12 -0700

    Added copyright headings in the mmitssLocal scripts. Developed a new script to create a macvlan network for the docker containers(issue#12).

--- docker/applications/lcleanstop.sh
+++ docker/applications/lcleanstop.sh
@@ -1,4 +1,29 @@
-#This script is for stopping all the started MMITSS processes on local machine, and delete the log files generated in the last run.
+#############################################################################################
+#                                                                                           #
+# NOTICE:  Copyright 2018 Arizona Board of Regents on behalf of University of Arizona.      #
+# All information, intellectual, and technical concepts contained herein is and shall       #
+# remain the proprietary information of Arizona Board of Regents and may be covered         #
+# by U.S. and Foreign Patents, and patents in process.  Dissemination of this information   #       
+# or reproduction of this material is strictly forbidden unless prior written permission    #
+# is obtained from Arizona Board of Regents or University of Arizona.                       #
+#                                                                                           #
+# lcleanstop.sh                                                                             #
+# Created by Niraj Altekar                                                                  #
+# Transportation Research Institute                                                         #
+# Systems and Industrial Engineering                                                        #
+# The University of Arizona                                                                 #
+#                                                                                           #
+# This code was develop under the supervision of Professor Larry Head                       #
+# in the Transportation Research Institute.                                                 #
+#                                                                                           #
+# Revision History:                                                                         #
+# Rev00: Initial Release.                                                                   #
+# This script is for stopping all the started MMITSS processes on local machine,            # 
+# and delete the log files generated in the last run.                                       #
+#                                                                                           #
+#############################################################################################
+
+
 #Maintainer: Niraj Altekar
 
 echo "Stopping MapSpatBroadcaster and MapSpatReceiver..."
