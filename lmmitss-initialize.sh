311bf028521a721ddacd61bd299356c758c311fd
pearson10m <pearson10m@gmail.com>
2019-11-15 13:34:41 -0700

    git commit -a -m

--- lmmitss-initialize.sh
+++ lmmitss-initialize.sh
@@ -31,17 +31,19 @@
 #*********************************************************************************************
 
 
-#Request the user-name and user-group
+#Request the user-name, user-group, and architecture
 read -p "Username: " username
 read -p "User Group: " usergroup
+read -p "Architecture - x86 or arm: " architecture
+
 
 echo "Creating required directories in the root folder."
 sudo mkdir -p /nojournal/bin/log
 sudo mkdir /usr/local/lib/mmitss
 sleep 1s
 
-echo "Copy the configuration files of the intersection RSE82_mountain to /nojournal/bin/"
-sudo cp -r ./docker/corridors/speedway/rse82_mountain/nojournal/bin /nojournal
+echo "Copy the configuration files of the intersection RSE81_Campbell to /nojournal/bin/"
+sudo cp -r ./docker/corridors/speedway/rse81_campbell/nojournal/bin /nojournal
 sleep 1s
 
 echo "Change the owner and group of the configuration files and provide necessary permissions (chmod 777)"
@@ -50,21 +52,43 @@ sudo chmod -R 777 /nojournal
 sleep 1s
 
 echo "Add the shared libraries we need to run"
-sudo cp ./3rdparty/net-snmp/lib/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
-sudo cp ./3rdparty/glpk/lib/libglpk.so.35.1.0 /usr/local/lib/mmitss/
-sudo cp ./lib/libmmitss-common.so /usr/local/lib/mmitss/
-sudo cp ./3rdparty/mapengine/lib/liblocAware.so.1.0 /usr/local/lib/mmitss/
-sudo cp ./3rdparty/asn1j2735/lib/libasn.so.1.0 /usr/local/lib/mmitss/
-sudo cp ./3rdparty/asn1j2735/lib/libdsrc.so.1.0 /usr/local/lib/mmitss/
+if architecture = x86
+then
+sudo cp ./3rdparty/net-snmp/lib/x86/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/glpk/lib/x86/libglpk.so.35.1.0 /usr/local/lib/mmitss/
+sudo cp ./lib/x86/libmmitss-common.so /usr/local/lib/mmitss/
+sudo cp ./3rdparty/mapengine/lib/x86/liblocAware.so.1.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/asn1j2735/lib/x86/libasn.so.1.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/asn1j2735/lib/x86/libdsrc.so.1.0 /usr/local/lib/mmitss/
 sudo cp ./lib/mmitss.conf /etc/ld.so.conf.d/
-sleep 1s
-
 echo "Create the symbolic links for the copied libraries."
 sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
 sudo ln -s /usr/local/lib/mmitss/libglpk.so.35.1.0 /usr/local/lib/mmitss/libglpk.so.35
 sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
 sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
 sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+fi
+
+if architecture = arm
+then
+sudo cp ./3rdparty/net-snmp/lib/arm/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/glpk/lib/arm/libglpk.so.40.3.0 /usr/local/lib/mmitss/
+sudo cp ./lib/arm/libmmitss-common.so /usr/local/lib/mmitss/
+sudo cp ./3rdparty/mapengine/lib/arm/liblocAware.so.1.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/asn1j2735/lib/arm/libasn.so.1.0 /usr/local/lib/mmitss/
+sudo cp ./3rdparty/asn1j2735/lib/arm/libdsrc.so.1.0 /usr/local/lib/mmitss/
+sudo cp ./lib/mmitss.conf /etc/ld.so.conf.d/
+echo "Create the symbolic links for the copied libraries."
+sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
+sudo ln -s /usr/local/lib/mmitss/libglpk.so.40.3.0 /usr/local/lib/mmitss/libglpk.so.40
+sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
+sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
+sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+fi
+
+sleep 1s
+
+
 
 sudo ldconfig
 pkill -9 sleep #End
