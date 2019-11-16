83226bb37cefe1d79298e47e28060cd3fe6f21f4
Larry Head <klhead@email.arizona.edu>
2019-11-15 17:49:45 -0700

    in progress. Moved BasicVehicle and position to local for now
    Changed BasicVehicle to return dict as well as json (seperate functions)

--- src/obu/controllerSimulatorforHMI/MMITSS-hmi-controller-simulator.py
+++ src/obu/controllerSimulatorforHMI/MMITSS-hmi-controller-simulator.py
@@ -22,8 +22,8 @@ import json
 import time
 import os
 
-from src.common.Position3D import Position3D
-from src.common.BasicVehicle import BasicVehicle
+from Position3D import Position3D
+from BasicVehicle import BasicVehicle
 
 
 controllerIP = '127.0.0.1'
@@ -41,9 +41,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the created socket to the server information.
 s.bind((controller))
 
-f = open('/User/Larry/Desktop/dev/mmitss-private/mmitss/src/obu/controllerSimulatorforHMI/HMIControllerSimulatorData.1.csv', 'r')
+directory_path = os.getcwd()
+f = open(directory_path + '/src/obu/controllerSimulatorforHMI/HMIControllerSimulatorData.1.csv', 'r')
 
 f.readline()
+f.readline() 
+f.readline() # there are three informational lines at the top of the data file (top is category, second is data_array cound, third is data lable)
 while (f.readline()):
     line = f.readline()
     line = line.replace('\n','')
@@ -51,88 +54,80 @@ while (f.readline()):
     secMark = int(data_array[0])
     #this is all vehicle information
     print("second = ", secMark)
-    tempID = int(data_array[1])
-    latitude_DecimalDegree= float(data_array[2])
-    longitude_DecimalDegree= float(data_array[3])
-    elevation_Meter= float(data_array[4])
-    heading_Degree= float(data_array[5])
-    speed_Meterpersecond= float(data_array[6])
-    speed_mph= round((float(speed_Meterpersecond) * 2.23694),2)
-    #vehicleType= "Truck"
-    #vehicleLane= int(data_array[6])
-
+    hv_tempID = int(data_array[1])
+    hv_vehicleType = data_array[2]
+    hv_latitude_DecimalDegree= float(data_array[32])
+    hv_longitude_DecimalDegree= float(data_array[4])
+    hv_elevation_Meter= float(data_array[5])
+    hv_heading_Degree= float(data_array[6])
+    hv_speed_Meterpersecond= float(data_array[7])
+    hv_speed_mph= round((float(hv_speed_Meterpersecond) * 2.23694),2)
+   
     # Create a Basic Vehicle that represents the host vehicle
-    positon = Position3D(latitude_DecimalDegree, longitude_DecimalDegree, elevation_Meter)
-    hostVehicle = BasicVehicle(tempID, secMark, position, speed_mph, heading_Degree)
+    hv_position = Position3D(hv_latitude_DecimalDegree, hv_longitude_DecimalDegree, hv_elevation_Meter)
+    hostVehicle = BasicVehicle(hv_tempID, secMark, hv_position, hv_speed_mph, hv_heading_Degree, 'transit')
 
-    #this is all reduced speed zone information
-    numOtherVehicles = int(data_array[7])
+    #this is all remote vehicle information
+    numRemoteVehicles = int(data_array[8])
     
-    statusOfLanes = []
-    for lane in range(0, int(numLanes)):
-        laneStatus = LaneStatus(lane)
+    remoteVehicles = []
+    for vehicle in range(0, 5): # assuming up to 5 remote vehicles for now
+        rv_tempID = data_array[9 + vehicle*7]
+        rv_vehicleType = data_array[10 + vehicle*7]
+        rv_latitude_DecimalDegree= float(data_array[11 + vehicle*7])
+        rv_longitude_DecimalDegree= float(data_array[12 + vehicle*7])
+        rv_elevation_Meter= float(data_array[13 + vehicle*7])
+        rv_heading_Degree= float(data_array[14 + vehicle*7])
+        rv_speed_Meterpersecond= float(data_array[15 + vehicle*7])
+        rv_speed_mph= round((float(rv_speed_Meterpersecond) * 2.23694),2)
+
+        rv_position = Position3D(rv_latitude_DecimalDegree, rv_longitude_DecimalDegree, rv_elevation_Meter)
+        remoteVehicle = BasicVehicle(rv_tempID, secMark, rv_position, rv_speed_mph, rv_heading_Degree, rv_vehicleType)
         
-        laneStatus.lane = int(data_array[7 + lane*8 + 1])
-        
-        laneStatus.laneSpeedLimit_mph= float(data_array[7 + lane*8 + 2])
-        laneStatus.laneSpeedChangeDistance = float(data_array[7 + lane*8 + 3])
-        
-        laneStatus.workersPresent = bool_map[data_array[7 + lane*8 + 4]]
+        if vehicle < numRemoteVehicles:
+             bv_dict = remoteVehicle.BasicVehicle2Dict()
+             remoteVehicles.append(bv_dict)
 
-        laneStatus.laneClosed = bool_map[data_array[7 + lane*8 + 5]]
-        laneStatus.laneClosedDistance = float(data_array[7 + lane*8 + 6])
-        laneStatus.taperRight = bool_map[data_array[7 + lane*8 + 7]]
-        laneStatus.taperLeft = bool_map[data_array[7 + lane*8 + 8]]
 
-        statusOfLanes.append(laneStatus.laneStatus2dict())
-
-    activeEventID = 'activeEventID'
-    activeDescriptiveName = 'activeDescriptiveName'
-
-    eventID1 = 'eventID1'
-    eventID2 = 'eventID2'
-    eventID3 = 'eventID3'
-    eventID4 = 'eventID4'
-
-    descriptiveName1 = "descriptiveName1"
-    descriptiveName2 = "descriptiveName2"
-    descriptiveName3 = "descriptiveName3"
-    descriptiveName4 = "descriptiveName4"
-
-    activeMap = dict({"eventID": activeEventID, "descriptiveName": activeDescriptiveName})
+ # infrastructure map data
     availableMaps = dict({"availableMaps":[                        
-                            {"eventID": eventID1, "descriptiveName": descriptiveName1},
-                            {"eventID": eventID2, "descriptiveName": descriptiveName2},
-                            {"eventID": eventID3, "descriptiveName": descriptiveName3},
-                            {"eventID": eventID4, "descriptiveName": descriptiveName4}
+                            {"IntersectionID": 101, "DescriptiveName": "Daisy Mountain and Gavilan Peak", "active": False, "age" : 100},
+                            {"IntersectionID": 102, "DescriptiveName": "Daisy Mountain and Dedication", "active": False, "age" : 200},
+                            {"IntersectionID": 103, "DescriptiveName": "Gavilan Peak and Boulder Creek High School", "active": True, "age" : 0},
+                            {"IntersectionID": 104, "DescriptiveName": "Gavilan Peak and Memorial Way", "active": False, "age" : 333},
+                            {"IntersectionID": 105, "DescriptiveName": "Daisy Mountain and Hastings", "active": False, "age": 270 }
                         ]})
 
     interfaceJsonString = json.dumps({
-        "controller_hmi_interface":
+        "mmitss_hmi_interface":
         {
-            "BasicVehicle" :
+            "hostVehicle" :
             {
-                "heading_Degree" : heading_Degree,
+                "heading_Degree" : hv_heading_Degree,
                 "position" :
                 {
-                    "elevation_Meter" : elevation_Meter,
-                    "latitude_DecimalDegree" : latitude_DecimalDegree,
-                    "longitude_DecimalDegree" : longitude_DecimalDegree
+                    "elevation_Meter" : hv_elevation_Meter,
+                    "latitude_DecimalDegree" : hv_latitude_DecimalDegree,
+                    "longitude_DecimalDegree" : hv_longitude_DecimalDegree
                 },
                 "secMark_Second" : secMark,
-                "speed_MeterPerSecond" : speed_Meterpersecond,
-                "temporaryID" : 1,
-                "vehicleType" : 100,
-                "lane": vehicleLane,
-                "speed_mph": speed_mph
+                "speed_MeterPerSecond" : hv_speed_Meterpersecond,
+                "temporaryID" : hv_tempID,
+                "vehicleType" : hv_vehicleType,
+                "lane": 1, # this is desireable data
+                "speed_mph": hv_speed_mph
             },
-            "mapCache":
+            "remoteVehicles" :
+                remoteVehicles,
+            
+            "infrastructure": 
             {
-                "activeMap": activeMap,
+                 "mapCache":
+            {
                 "availableMaps": availableMaps
             },
-            "lanes":
-                statusOfLanes #this is a list of lane dictionaries
+            }
+            
         }
     })
     s.sendto(interfaceJsonString.encode(),hmi)
