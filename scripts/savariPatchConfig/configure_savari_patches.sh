44c58f4eb16699f6d0ad22f1a5c6e40ffa8d0821
nvaltekar <nvaltekar@email.arizona.edu>
2019-11-22 14:05:43 -0700

    Created a static variable for the IPAddress

--- scripts/savariPatchConfig/configure_savari_patches.sh
+++ scripts/savariPatchConfig/configure_savari_patches.sh
@@ -4,3 +4,4 @@ read -p "Enter the IP address of the RaspberryPi associated with this OBU: " ras
 sed -i 's/IPADDRESS/'${raspiIp}'/g' DsrcForward.conf v2vi_obe.conf
 cp DsrcForward.conf /etc/config/
 cp v2vi_obe.conf /etc/config
+sed -i 's/'${raspiIp}'/IPADDRESS/g' DsrcForward.conf v2vi_obe.conf
