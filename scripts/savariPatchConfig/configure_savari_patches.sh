7d640b9f889c82c8c7df7ba365984da5b0f604b0
nvaltekar <nvaltekar@email.arizona.edu>
2019-11-22 13:49:37 -0700

    Added scripts for configuring Savari patches and running them

---
+++ scripts/savariPatchConfig/configure_savari_patches.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter the IP address of the RaspberryPi associated with this OBU: " raspiIp
+sed -i 's/IPADDRESS/'${raspiIp}'/g' DsrcForward.conf v2vi_obe.conf
+cp DsrcForward.conf /etc/config/
+cp v2vi_obe.conf /etc/config
