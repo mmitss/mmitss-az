14be93232a2dcefbd8b22831c8c467d784d637e0
debipe20 <debashisdas@email.arizona.edu>
2019-11-24 11:28:50 -0700

    PRG, TransceiverDecoder, TransceiverEncoder is modified. PRG will get the port info from config file. Encoder will iterate getMsgType() method once per message, few warnings related to socket is removed

--- src/common/MsgTransceiver/MsgEncoder/main.cpp
+++ src/common/MsgTransceiver/MsgEncoder/main.cpp
@@ -6,60 +6,53 @@
 #include "json/json.h"
 
 int main()
-{ 
+{
     Json::Value jsonObject_config;
-	Json::Reader reader;
-	std::ifstream configJson("/nojournal/bin/mmitss-phase3-master-config.json");
+    Json::Reader reader;
+    std::ifstream configJson("/nojournal/bin/mmitss-phase3-master-config.json");
     std::string configJsonString((std::istreambuf_iterator<char>(configJson)), std::istreambuf_iterator<char>());
-	reader.parse(configJsonString.c_str(), jsonObject_config);
+    reader.parse(configJsonString.c_str(), jsonObject_config);
 
-
-    UdpSocket encoderSocket(jsonObject_config["PortNumber"]["MessageTransceiver"]["MessageEncoder"].asInt());
+    UdpSocket encoderSocket(static_cast<short unsigned int>(jsonObject_config["PortNumber"]["MessageTransceiver"]["MessageEncoder"].asInt()));
     TransceiverEncoder encoder;
     std::string messagePayload{};
     char receiveBuffer[5120];
     const string LOCALHOST = jsonObject_config["HostIp"].asString();
     const int messageSenderPortNo = (jsonObject_config["PortNumber"]["MessageTransceiver"]["MessageSender"]).asInt();
- 
-    while(true)
+
+    while (true)
     {
         encoderSocket.receiveData(receiveBuffer, sizeof(receiveBuffer));
         std::string jsonString(receiveBuffer);
-        
-        if (encoder.getMessageType(jsonString) == MsgEnum::DSRCmsgID_bsm)
-        {
-    
-            messagePayload = encoder.BSMEncoder(jsonString);           
-            encoderSocket.sendData(LOCALHOST, messageSenderPortNo,messagePayload);
-            std::cout << "Encoded BSM" << std::endl;
-            
-        }
-       
-        else if (encoder.getMessageType(jsonString) == MsgEnum::DSRCmsgID_srm)
-        {
+        int msgType = encoder.getMessageType(jsonString);
 
-            messagePayload = encoder.SRMEncoder(jsonString);  
-            encoderSocket.sendData(LOCALHOST, messageSenderPortNo,messagePayload);
+        if (msgType == MsgEnum::DSRCmsgID_bsm)
+        {
+            messagePayload = encoder.BSMEncoder(jsonString);
+            encoderSocket.sendData(LOCALHOST, static_cast<short unsigned int>(messageSenderPortNo), messagePayload);
+            std::cout << "Encoded BSM" << std::endl;
+        }
+
+        else if (msgType == MsgEnum::DSRCmsgID_srm)
+        {
+            messagePayload = encoder.SRMEncoder(jsonString);
+            encoderSocket.sendData(LOCALHOST, static_cast<short unsigned int>(messageSenderPortNo), messagePayload);
             std::cout << "Encoded SRM" << std::endl;
         }
 
-        else if (encoder.getMessageType(jsonString) == MsgEnum::DSRCmsgID_spat)
+        else if (msgType == MsgEnum::DSRCmsgID_spat)
         {
-          
             messagePayload = encoder.SPaTEncoder(jsonString);
-            encoderSocket.sendData(LOCALHOST, messageSenderPortNo,messagePayload);
+            encoderSocket.sendData(LOCALHOST, static_cast<short unsigned int>(messageSenderPortNo), messagePayload);
             std::cout << "Encoded SPAT" << std::endl;
-            
         }
 
-        else if (encoder.getMessageType(jsonString) == MsgEnum::DSRCmsgID_ssm)
-        {   
+        else if (msgType == MsgEnum::DSRCmsgID_ssm)
+        {
             messagePayload = encoder.SSMEncoder(jsonString);
-            encoderSocket.sendData(LOCALHOST, messageSenderPortNo,messagePayload);
+            encoderSocket.sendData(LOCALHOST, static_cast<short unsigned int>(messageSenderPortNo), messagePayload);
             std::cout << "Encoded SSM" << std::endl;
-
-        }        
-        
+        }
     }
     return 0;
 }
