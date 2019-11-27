64e98b66836d4faea976b0c3c4329d519160c975
Larry Head <klhead@email.arizona.edu>
2019-11-27 07:07:54 -0700

    created bsm-spat simulator for sending data to the hmi-controller

---
+++ src/obu/controllerSimulatorforHMI/mmitss-hmi-remote-bsm-spat-simulator.py
@@ -0,0 +1,158 @@
+'''
+**********************************************************************************
+ © 2019 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+**********************************************************************************
+  MMITSS-host-prg-simulator.py
+  Created by: Larry Head
+  University of Arizona   
+  College of Engineering
+
+  
+  Operational Description:
+
+  This application does the following tasks:
+  1. Reads host vehicle and prg data from a .csv file 
+  2. Builds json representing the structure sent from prg to mmitss-hmi-controller
+  3. Sends the json to the mmitss-hmi-controller
+'''
+
+import socket
+import json
+import time
+import os
+
+from Position3D import Position3D
+from BasicVehicle import BasicVehicle
+
+hmi_controllerIP = '127.0.0.1'
+hmi_controllerPort = 20009
+hmi_controller = (hmi_controllerIP, hmi_controllerPort)
+
+bsm_spat_simIP = '127.0.0.1'
+bsm_spat_simPort = 10004
+bsm_spat_sim = (bsm_spat_simIP, bsm_spat_simPort)
+
+bool_map = {"TRUE": True, "True": True, "FALSE": False, "False": False} # this could be come the SPaT phaseStatus data map
+spat_state = {0 : "unknown", # based on the MOvementPhaseState from the SAE J2735 2016 standard - not comment in MovementPhaseState is that these are not used with UPER encoding (???)
+              1 : "dark", 
+              2 : "stop-Then-Proceed", # flashing red (flashing Red ball)
+              3 : "stop-And-Remain", # red light (Red ball) [Don't walk]
+              4 : "pre-Movement", # not used in US
+              5 : "permissive-Movement-Allowed", # permissive green (Green ball)
+              6 : "protected-Movement-Allowed",  # protected green (e.g. left turn arrow) - Green Arrow (direction?) [also walk]
+              7 : "permissive-clearance", # permissive yellow (clear intersection) - Yellow 
+              8 : "protected-clearance", # protected yellow (clear intersection) - Yellow arrow  [ also ped clear= Flashing Don;t Walk]
+              9 : "caution-Conflicting-Traffic", # flashing yellow (yield)
+              } 
+spat_signal_head = {"stop-And-Remain" : "red", "stop-Then-Proceed" : "red_flash", "protected-Movement-Allowed" : "green", "permissive-clearance" : "yellow", "protected-clearance" : "yellow",  "dark" : "dark"}
+
+
+priority_responseStatus = {0 : "unknown", 
+                           1 : "requested",
+                           2 : "processing",
+                           3 : "watchOtherTraffic",
+                           4 : "granted",
+                           5 : "rejected",
+                           6 : "maxPresence",
+                           7 : "reserviceLocked"}
+
+basicVehicleRoles = {0 : "basicVehicle",
+                    9 : "truck",
+                    13 : "ev-fire",
+                    16 : "transit"}
+
+# Create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the created socket to the server information.
+s.bind((bsm_spat_sim))
+
+directory_path = os.getcwd()
+f = open(directory_path + '/mmitss/src/obu/controllerSimulatorforHMI/HMIControllerSimulatorData.1.csv', 'r')
+
+ticks_init = time.time()
+
+f.readline() 
+f.readline() # there are three informational lines at the top of the data file (top is category, second is data_array cound, third is data lable)
+while (f.readline()):
+    line = f.readline()
+    line = line.replace('\n','')
+    data_array = line.split(',')
+    secMark = int(data_array[0])
+   
+    index_remoteVehicle = 10
+    numRemoteVehicles = int(data_array[index_remoteVehicle])
+
+    for vehicle in range(0, numRemoteVehicles): # assuming up to 5 remote vehicles for now
+        rv_tempID = data_array[index_remoteVehicle + vehicle*7]
+        rv_vehicleType = data_array[index_remoteVehicle + 2 + vehicle*7]
+        rv_latitude_DecimalDegree= round(float(data_array[index_remoteVehicle + 3 + vehicle*7]), 8)
+        rv_longitude_DecimalDegree= round(float(data_array[index_remoteVehicle + 4 + vehicle*7]), 8)
+        rv_elevation_Meter= round(float(data_array[index_remoteVehicle + 5 + vehicle*7]), 1)
+        rv_heading_Degree= round(float(data_array[index_remoteVehicle + 6 + vehicle*7]), 4)
+        rv_speed_Meterpersecond= float(data_array[index_remoteVehicle + 7 + vehicle*7])
+        rv_speed_mph= int((float(rv_speed_Meterpersecond) * 2.23694))
+        
+        if vehicle < numRemoteVehicles:          
+            interfaceJsonString = json.dumps({
+                "BSM": 
+                    {
+                        "BasicVehicle" : 
+                        {
+                            "temporaryID" : rv_tempID,
+                            "secMark_Second" : secMark,
+                            "position" : 
+                                { 
+                                    "latitude_DecimalDegree" : rv_latitude_DecimalDegree,
+                                    "longitude_DecimalDegree" : rv_longitude_DecimalDegree,
+                                    "elevation_Meter" : rv_elevation_Meter
+                                },
+                            "speed_MeterPerSecond" : rv_speed_Meterpersecond,
+                            "heading_Degree" : rv_heading_Degree,
+                            "type" : rv_vehicleType
+                        }
+                    },
+            })
+            s.sendto(interfaceJsonString.encode(),hmi_controller)
+            print("sent remote bsm at time: ", time.time() - ticks_init)
+            time.sleep(0.1/(numRemoteVehicles+1))
+
+    #infrastructure SPaT data
+    #signal phase status 
+    numSPaT = 8 # currently we have one SPaT value for each signal phase. 
+    index_spat = 67
+    SPaT = []
+    #spat_currentPhase = int(data_array[index_spat]) - 1 # phases are stored 0 to 7 (instead of 1 to 8)
+    for spat in range(0, numSPaT):
+        spat_phase = data_array[index_spat + spat*6]
+        spat_currState = spat_state[int(data_array[index_spat + 1 + spat*6])]
+        spat_startTime = round(float(data_array[index_spat + 2 + spat*6])/10., 1) # starttime is in 10ths of a second - show only one decimal point
+        spat_minEndTime = round(float(data_array[index_spat + 3 + spat*6])/10., 1) # minEndTime is in 10ths of a second
+        spat_maxEndTime = round(float(data_array[index_spat + 4 + spat*6])/10., 1) # maxEndTime is in 10ths of a second
+        spat_elapsedTime = round(float(data_array[index_spat + 5 + spat*6])/10., 1) # elapsedTime is in 10ths of a second 
+        SPaT.append({"phase" : spat_phase, "currState" : spat_currState, "minEndTime" : spat_minEndTime, "maxEndTime": spat_maxEndTime})
+
+    #ped phase status
+    numSPaTPed = 8 # currently we have one ped for each phase, but only 2, 4, 6, and 8 are real peds
+    pedSPaT = []
+    index_ped_spat = 115
+    for spat in range(0, numSPaT):
+        spat_phase = data_array[index_ped_spat + spat*6]
+        spat_currState = spat_state[int(data_array[index_ped_spat + 1 + spat*6])]
+        spat_startTime = round(float(data_array[index_ped_spat + 2 + spat*6])/10., 1) # starttime is in 10ths of a second - show only one decimal point
+        spat_minEndTime = round(float(data_array[index_ped_spat + 3 + spat*6])/10., 1) # minEndTime is in 10ths of a second
+        spat_maxEndTime = round(float(data_array[index_ped_spat + 4 + spat*6])/10., 1) # maxEndTime is in 10ths of a second
+        spat_elapsedTime = round(float(data_array[index_ped_spat + 5 + spat*6])/10., 1) # elapsedTime is in 10ths of a second 
+        pedSPaT.append({"phase" : spat_phase, "currState" : spat_currState, "minEndTime" : spat_minEndTime, "maxEndTime": spat_maxEndTime})
+
+    interfaceJsonString = json.dumps({
+            "infrastructure": 
+            {
+                "SPaT" : SPaT,
+                "pedSPaT" : pedSPaT
+            },
+    })
+    s.sendto(interfaceJsonString.encode(),hmi_controller)
+    print("sent spat at time: ", time.time() - ticks_init)
+    time.sleep(0.1/(numRemoteVehicles+1))
+s.close() 
