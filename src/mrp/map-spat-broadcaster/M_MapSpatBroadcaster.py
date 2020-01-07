4b7e7c995a14f15c232045beb920f53590c56736
nvaltekar <nvaltekar@email.arizona.edu>
2020-01-07 13:35:31 -0700

    Added SNMP check in the console output if we are not receiving SPAT message

--- src/mrp/map-spat-broadcaster/M_MapSpatBroadcaster.py
+++ src/mrp/map-spat-broadcaster/M_MapSpatBroadcaster.py
@@ -93,7 +93,7 @@ def main():
                     print("Sent MAP to MsgSender")
                 #print("Sent SPaT JSON to msgEncoder and trafficControllerObserver, and MAP payload to msgSender.")
         except socket.timeout:
-            print("No packets received from the Traffic Signal Controller. Check:\n1. Physical connection between CVCP and Traffic Signal Controller.\n2. Server IP in MM-1-5-1 of the Signal Controller must match the IP address of CVCP.\n3. Address in MM-1-5-3 must be set to 6053.\n4. Controller must be power-cycled after changes in internal configuration.\n")
+            print("No packets received from the Traffic Signal Controller. Check:\n1. Physical connection between CVCP and Traffic Signal Controller.\n2. Server IP in MM-1-5-1 of the Signal Controller must match the IP address of CVCP.\n3. Address in MM-1-5-3 must be set to 6053.\n4. Controller must be power-cycled after changes in internal configuration.\n5. Controller must be set to broadcast spat blobs using SNMP interface. asc3ViiMessageEnable or '1.3.6.1.4.1.1206.3.5.2.9.44.1.1' must equal 6.")
             print("Sent MAP to MsgSender")
             outerSocket.sendto(mapPayload.encode(), msgSenderAddress)
             spatMapMsgCount = 0
