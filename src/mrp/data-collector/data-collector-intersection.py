d4960d640228d5c60d82d8b38a34449014daebe4
nvaltekar <nvaltekar@email.arizona.edu>
2020-01-09 18:45:59 -0700

    CyVerse streaming stable for BSMs and SPAT

--- src/mrp/data-collector/data-collector-intersection.py
+++ src/mrp/data-collector/data-collector-intersection.py
@@ -28,28 +28,30 @@ def main():
     dataCollectorAddress = (hostIp, port)
     s.bind(dataCollectorAddress)
 
-    logFileCreationDay = datetime.datetime.now().day
+    logFileCreationDay = datetime.datetime.now().minute
 
+    currentSpatFilename = "spatLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"
     spatLogFile = open(("spatLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
     DCM.initializeSpatLogFile(spatLogFile)
     spatLogCounter = 1
 
-    surroundingBsmLogFile = open(("surroundingBsmLog_" + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+    currentSurroundingBsmFilename = "surroundingBsmLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"
+    surroundingBsmLogFile = open((currentSurroundingBsmFilename), 'w')
     DCM.initializeBsmLogFile(surroundingBsmLogFile)
     surroundingBsmLogCounter = 1
 
     while True:
-        currentDay = datetime.datetime.now().day
+        currentDay = datetime.datetime.now().minute
         if currentDay == logFileCreationDay:
             data, address = s.recvfrom(4096)
             jsonData = json.loads(data.decode())
-            if address[1] == config["PortNumber"]["MapSPaTBroadcaster"]: # then this message is a SPAT message
+            if jsonData["MsgType"]=="SPAT": # then this message is a SPAT message
                 spatLogFile.write(DCM.spatJsonToCsv(jsonData))
                 spatLogCounter = spatLogCounter+1
                 if spatLogCounter > flushLogFileEvery: # flush every 1 minutes
                     spatLogFile.flush()
                     spatLogCounter = 1
-            elif address[1] == config["PortNumber"]["MessageTransceiver"]["MessageDecoder"]: # then this message is a bsm message from 
+            elif jsonData["MsgType"]=="BSM": # then this message is a bsm message from 
                 surroundingBsmLogFile.write(DCM.bsmJsonToCsv(jsonData))
                 surroundingBsmLogCounter = surroundingBsmLogCounter+1
                 if surroundingBsmLogCounter > flushLogFileEvery: # flush every 1 minutes
@@ -62,30 +64,40 @@ def main():
             spatLogFile.close()
 
             # Interaction with CyVerse:
-            sh.icd(CyVerse_DirectoryPath_Spat) # Go to correct directory for storing SPAT data
-            sh.iput("./spatLog_" + IntersectionName + "*") # Upload all files matching to current directory
-            sh.rm("./spatLog_*") # Remove the file from local storage
+            sh.icd(CyVerse_DirectoryPath_Spat) # Go to correct CyVerse directory for storing SPAT data
+            sh.iput(currentSpatFilename) # Upload all files of this intersection to current directory of CyVerse
+            sh.rm(currentSpatFilename) # Remove the files of this intersection from local storage
 
             # Open a new log file with new timestamp and initialize it.
-            spatLogFile = open(("spatLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+            currentSpatFilename = "spatLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"
+            spatLogFile = open(currentSpatFilename, 'w')
             DCM.initializeSpatLogFile(spatLogFile)
 
-
+            # Close the file so that it can be transferred to CyVerse and deleted from local file system            
             surroundingBsmLogFile.close()
-            surroundingBsmLogFile = open(("surroundingBsmLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+
+            # Interaction with CyVerse:
+            sh.icd(CyVerse_DirectoryPath_Spat) # Go to correct directory for storing SPAT data
+            sh.iput(currentSurroundingBsmFilename) # Upload all files matching to current directory
+            sh.rm(currentSurroundingBsmFilename) # Remove the file from local storage            
+
+            # Open a new log file with new timestamp and initialize it.
+            currentSurroundingBsmFilename = "surroundingBsmLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"
+            surroundingBsmLogFile = open((currentSurroundingBsmFilename), 'w')
             DCM.initializeBsmLogFile(surroundingBsmLogFile)
+            
             data, address = s.recvfrom(4096)
             jsonData = json.loads(data.decode())
-            if address[1] == config["PortNumber"]["MapSPaTBroadcaster"]: # then this message is a SPAT message
+            if jsonData["MsgType"]=="SPAT": # then this message is a SPAT message
                 spatLogFile.write(DCM.spatJsonToCsv(jsonData))
                 spatLogCounter = spatLogCounter+1
-                if spatLogCounter > flushLogFileEvery: # flush every 1 minutes
+                if spatLogCounter > flushLogFileEvery: # flush every 1 minutes calculated using the frequency of received message
                     spatLogFile.flush()
                     spatLogCounter = 1
-            elif address[1] == config["PortNumber"]["MessageTransceiver"]["MessageDecoder"]: # then this message is a bsm message from 
+            elif jsonData["MsgType"]=="BSM": # then this message is a bsm message from 
                 surroundingBsmLogFile.write(DCM.bsmJsonToCsv(jsonData))
                 surroundingBsmLogCounter = surroundingBsmLogCounter+1
-                if surroundingBsmLogCounter > flushLogFileEvery: # flush every 1 minutes
+                if surroundingBsmLogCounter > flushLogFileEvery: # flush every 1 minutes 
                     surroundingBsmLogFile.flush()
                     surroundingBsmLogCounter = 1
 
