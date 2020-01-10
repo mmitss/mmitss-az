d1f48a66f06df27ed5c3f462b48334484e0f0c9f
nvaltekar <nvaltekar@email.arizona.edu>
2020-01-09 17:17:08 -0700

    import sh

--- src/mrp/data-collector/data-collector-intersection.py
+++ src/mrp/data-collector/data-collector-intersection.py
@@ -5,35 +5,41 @@ import os
 import DataCollectorMethods as DCM
 import glob
 import sys
+import sh
 
 appLocation = 1 # 0 = vehicle; 1 = infrastructure
 flushLogFileEvery = 6001 # Seconds
 
 def main():
 
-    intersectionConfig = open(sys.argv[1], 'r')
-    
-    configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
+    configFile = open(sys.argv[1], 'r')    
     config = (json.load(configFile))
+    IntersectionName = config["IntersectionName"]
+
+    ################## CyVerse Directory Paths ##################
+    CyVerse_DirectoryPath_Spat = config["CyVerse_DirectoryPath"]["Spat"]
+    CyVerse_DirectoryPath_SurroundingBsms = config["CyVerse_DirectoryPath"]["SurroundingBsms"]
+    CyVerse_DirectoryPath_PseudoSurroundingBsms = config["CyVerse_DirectoryPath"]["PseudoSurroundingBsms"]
+
     # Establish a socket and bind it to IP and port
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    hostIp = config["DataCollectorIP"]
-    port = config["PortNumber"]["DataCollector"]
+    hostIp = config["DataCollectorIp"]
+    port = config["DataCollectorPort"]
     dataCollectorAddress = (hostIp, port)
     s.bind(dataCollectorAddress)
 
-    logFileCreationDay = datetime.datetime.now().minute
+    logFileCreationDay = datetime.datetime.now().day
 
-    spatLogFile = open(("spatLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+    spatLogFile = open(("spatLog_" + IntersectionName + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
     DCM.initializeSpatLogFile(spatLogFile)
     spatLogCounter = 1
 
-    surroundingBsmLogFile = open(("surroundingBsmLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+    surroundingBsmLogFile = open(("surroundingBsmLog_" + "_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
     DCM.initializeBsmLogFile(surroundingBsmLogFile)
     surroundingBsmLogCounter = 1
 
     while True:
-        currentDay = datetime.datetime.now().minute
+        currentDay = datetime.datetime.now().day
         if currentDay == logFileCreationDay:
             data, address = s.recvfrom(4096)
             jsonData = json.loads(data.decode())
@@ -51,15 +57,19 @@ def main():
                     surroundingBsmLogCounter = 1
         else:
             logFileCreationDay = currentDay
+
+            # Close the file so that it can be transferred to CyVerse and deleted from local file system            
             spatLogFile.close()
 
+            # Interaction with CyVerse:
+            sh.icd(CyVerse_DirectoryPath_Spat) # Go to correct directory for storing SPAT data
+            sh.iput("./spatLog_" + IntersectionName + "*") # Upload all files matching to current directory
+            sh.rm("./spatLog_*") # Remove the file from local storage
 
-
+            # Open a new log file with new timestamp and initialize it.
             spatLogFile = open(("spatLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
             DCM.initializeSpatLogFile(spatLogFile)
-            os.system('icd /iplant/home/nvaltekar/')
-            os.system('iput ./spatLog*')
-            os.system('rm ./spatLog*')
+
 
             surroundingBsmLogFile.close()
             surroundingBsmLogFile = open(("surroundingBsmLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
