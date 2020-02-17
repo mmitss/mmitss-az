0d4e65f13bd2266125d630b5b7d5a711e8b7e804
nvaltekar <nvaltekar@email.arizona.edu>
2020-02-17 11:53:37 -0700

    updated launch scripts - removed launching of deleted components

--- scripts/mmitss_launch_all.sh
+++ scripts/mmitss_launch_all.sh
@@ -37,39 +37,9 @@ sleep 1s
 #######################################################################################
 
 #######################################################################################
-echo "Starting Message Sender..."
-cd ./../src/common/MsgTransceiver/MsgSender
-python3 M_MsgSender.py > /dev/null 2>&1 &
-
-# Return back to original directory to go over the process again for another one
-cd - &> /dev/null
-sleep 1s
-#######################################################################################
-
-#######################################################################################
-echo "Starting Wireless Message Receiver..."
-cd ./../src/common/MsgTransceiver/MsgReceiver
-python3 M_WirelessMsgReceiver.py > /dev/null 2>&1 &
-
-# Return back to original directory to go over the process again for another one
-cd - &> /dev/null
-sleep 1s
-#######################################################################################
-
-#######################################################################################
 echo "Starting Wireless Message Decoder..."
 cd ./../src/common/MsgTransceiver/MsgDecoder/WirelessMsgDecoder
 ./M_WirelessMsgDecoder > /dev/null 2>&1 &
-
-# Return back to original directory to go over the process again for another one
-cd - &> /dev/null
-sleep 1s
-#######################################################################################
-
-#######################################################################################
-echo "Starting Host BSM Receiver..."
-cd ./../src/common/MsgTransceiver/MsgReceiver
-python3 M_HostBsmReceiver.py > /dev/null 2>&1 &
 
 # Return back to original directory to go over the process again for another one
 cd - &> /dev/null
