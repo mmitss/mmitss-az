ebf7582466961126266aa9ca8b1c74bed910585f
nvaltekar <nvaltekar@email.arizona.edu>
2020-02-21 09:29:39 -0700

    Interim commit

--- src/mrp/traffic-controller-interface/Schedule.py
+++ src/mrp/traffic-controller-interface/Schedule.py
@@ -1,49 +1,62 @@
 import json
 from Command import Command
 
-scheduleFile = open("schedule.json", "r")
-scheduleJson = json.loads(scheduleFile.read())
+class Scheduler:
+    def __init__(self):
+        pass
 
-schedule = scheduleJson["Schedule"]
+    def processNewSchedule(self, scheduleJson:json):
 
-schedule = sorted(schedule, key = lambda i: (i["commandStartTime"], i["commandType"], i["commandEndTime"]))
+        # Sort the schedule by three levels: 1. Command Start Time, 2. Command Type, and 3. Command End Time
+        scheduleJson = scheduleJson["Schedule"]
+        scheduleJson = sorted(scheduleJson, key = lambda i: (i["commandStartTime"], i["commandType"], i["commandEndTime"]))
 
-# Read the json into a data structure
-commandsInSchedule = []
-for command in schedule:
-    commandObject = Command(command["commandPhase"], command["commandStartTime"], command["commandEndTime"], command["commandType"])
-    commandsInSchedule = commandsInSchedule + [commandObject]
+        # Read the json into a data structure
+        commandsInSchedule = []
+        for command in scheduleJson:
+            commandObject = Command(command["commandPhase"], command["commandStartTime"], command["commandEndTime"], command["commandType"])
+            commandsInSchedule = commandsInSchedule + [commandObject]
 
-# Form action group
-flag = True
-i = 0
-while i < len(commandsInSchedule):
-    currentGroup = [commandsInSchedule[i]]
+        # Form action group
+        i = 0
+        while i < len(commandsInSchedule):
+            currentGroup = [commandsInSchedule[i]]
 
-    # Look for all commands in the list that has same start time and the type
-    for command in commandsInSchedule:
-        if (command != currentGroup[0] and (command.startTime == currentGroup[0].startTime) and (command.commandType == currentGroup[0].commandType)):
-            currentGroup = currentGroup + [command]
-            i = i+1
+            # Look for all commands in the list that has same start time and the type
+            for command in commandsInSchedule:
+                if (command != currentGroup[0] and (command.startTime == currentGroup[0].startTime) and (command.commandType == currentGroup[0].commandType)):
+                    currentGroup = currentGroup + [command]
+                    i = i+1
+
+            groupCommand =  formulateGroupCommand(currentGroup)
+            print(groupCommand)
+            currentGroup = []
+            i = i + 1
     
-    groupCommand = currentGroup[0].commandType
-    groupStartTime = currentGroup[0].startTime
-    groupEndTime = currentGroup[0].endTime
-    groupPhases = []
-    for command in currentGroup:
-        groupPhases = groupPhases + [command.phase]
+        # Formulate group command:
+        def formulateGroupCommand(self, currentGroup:list):
+            groupCommand = currentGroup[0].commandType
+            groupStartTime = currentGroup[0].startTime
+            groupEndTime = currentGroup[0].endTime
+            groupPhases = []
+            for command in currentGroup:
+                groupPhases = groupPhases + [command.phase]
 
-    phaseStr = list("00000000")
-    for phase in groupPhases:
-        phaseStr[phase-1]="1"
-    phaseStr = phaseStr[::-1]
-    phaseStr = "".join(phaseStr)
-    phaseInt = int(phaseStr,2)
+            groupPhaseInt = formulateBinaryInegerRepresentation(groupPhases)
 
-    # Formulate the binary integer representation of the commandPhase:
-    
+            groupCommand = Command(groupPhaseInt, groupStartTime, groupEndTime, groupCommand)
+            return groupCommand
+        
+        # Formulate the binary integer representation of the commandPhase:
+        def formulateBinaryInegerRepresentation(self, groupPhases):
+            groupPhaseStr = list("00000000")
+            for phase in groupPhases:
+                groupPhaseStr[phase-1]="1"
+            groupPhaseStr = groupPhaseStr[::-1]
+            groupPhaseStr = "".join(groupPhaseStr)
+            groupPhaseInt = int(groupPhaseStr,2)
+            return groupPhaseInt    
 
-
-
-    currentGroup = []
-    i = i + 1
+    if __name__ == "__main__":
+        scheduleFile = open("schedule.json", "r")
+        scheduleJson = json.loads(scheduleFile.read())
