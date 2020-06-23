218ddfb393132f8daf46a6464e809e9812cbc545
debipe20 <debashisdas@email.arizona.edu>
2020-06-22 22:31:35 -0700

    docker pre-test

--- src/mrp/map-spat-broadcaster/M_MapSpatBroadcaster.py
+++ src/mrp/map-spat-broadcaster/M_MapSpatBroadcaster.py
@@ -85,38 +85,38 @@ def main():
     print("Waiting for packets received from the Traffic Signal Controller. Check:\n1. Physical connection between CVCP and Traffic Signal Controller.\n2. Server IP in MM-1-5-1 of the Signal Controller must match the IP address of CVCP.\n3. Address in MM-1-5-3 must be set to 6053.\n4. Controller must be power-cycled after changes in internal configuration.\n5. Controller must be set to broadcast spat blobs using SNMP interface. asc3ViiMessageEnable or '1.3.6.1.4.1.1206.3.5.2.9.44.1.1' must equal 6.")
 
     while True:
-        try:
-            spatBlob, addr = outerSocket.recvfrom(1024)     
-            # Send spat blob to external clients:       
-            if addr[0] == controllerIp:
-                for client in clients_spatBlob:
-                    address = (client["IP"], client["Port"])
-                    outerSocket.sendto(spatBlob, address)
-                        
-                currentBlob.processNewData(spatBlob)
-                if(msgCnt < 127):
-                    msgCnt = msgCnt + 1
-                else: msgCnt = 0
-                spatObject.setmsgCnt(msgCnt)
-                spatObject.fillSpatInformation(currentBlob)
-                spatJsonString = spatObject.Spat2Json()
-                currentPhasesJson = json.dumps(currentBlob.getCurrentPhasesDict())
-                outerSocket.sendto(spatJsonString.encode(), msgEncoderAddress)
-                outerSocket.sendto(spatJsonString.encode(), dataCollectorAddress)
-                outerSocket.sendto(currentPhasesJson.encode(), tci_currPhaseAddress)
-            
-                print("Sent SPAT to MsgEncoder at:" + str(time.time()))
-                
-                # Send spat json to external clients:
-                for client in clients_spatJson:
-                    address = (client["IP"], client["Port"])
-                    outerSocket.sendto(spatJsonString.encode(), address)
 
-                spatMapMsgCount = spatMapMsgCount + 1
-                if spatMapMsgCount > 9:
-                    outerSocket.sendto(mapPayload.encode(), msgEncoderAddress)
-                    spatMapMsgCount = 0
-                    print("Sent MAP to MsgEncoder at:" + str(time.time()))
+        spatBlob, addr = outerSocket.recvfrom(1024)     
+        # Send spat blob to external clients:       
+        if addr[0] == controllerIp:
+            for client in clients_spatBlob:
+                address = (client["IP"], client["Port"])
+                outerSocket.sendto(spatBlob, address)
+                    
+            currentBlob.processNewData(spatBlob)
+            if(msgCnt < 127):
+                msgCnt = msgCnt + 1
+            else: msgCnt = 0
+            spatObject.setmsgCnt(msgCnt)
+            spatObject.fillSpatInformation(currentBlob)
+            spatJsonString = spatObject.Spat2Json()
+            currentPhasesJson = json.dumps(currentBlob.getCurrentPhasesDict())
+            outerSocket.sendto(spatJsonString.encode(), msgEncoderAddress)
+            outerSocket.sendto(spatJsonString.encode(), dataCollectorAddress)
+            outerSocket.sendto(currentPhasesJson.encode(), tci_currPhaseAddress)
+        
+            print("Sent SPAT to MsgEncoder at:" + str(time.time()))
+            
+            # Send spat json to external clients:
+            for client in clients_spatJson:
+                address = (client["IP"], client["Port"])
+                outerSocket.sendto(spatJsonString.encode(), address)
+
+            spatMapMsgCount = spatMapMsgCount + 1
+            if spatMapMsgCount > 9:
+                outerSocket.sendto(mapPayload.encode(), msgEncoderAddress)
+                spatMapMsgCount = 0
+                print("Sent MAP to MsgEncoder at:" + str(time.time()))
 
 if __name__ == "__main__":
     main()
