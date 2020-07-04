9b2f1ef4e8a20729486f13ea6614c25e627bce64
nvaltekar <nvaltekar@email.arizona.edu>
2020-07-04 18:08:44 +0530

    added a function to log on demand

--- src/simulation/message-distributor/M_MessageDistributor.py
+++ src/simulation/message-distributor/M_MessageDistributor.py
@@ -20,12 +20,14 @@ import json
 import socket
 import sys
 from MessageDistributor import MessageDistributor
+import datetime
 
-configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
+configFile = open("../../../bin/corridors/simulation-tools/nojournal/bin/mmitss-phase3-master-config.json", 'r')
 masterConfig = json.load(configFile)
 configFile.close()
 
-configFile = open("/nojournal/bin/mmitss-message-distributor-config.json", 'r')
+#configFile = open("/nojournal/bin/mmitss-message-distributor-config.json", 'r')
+configFile = open("../../../bin/corridors/simulation-tools/nojournal/bin/mmitss-message-distributor-config.json", 'r')
 config = json.load(configFile)
 configFile.close()
 
@@ -34,6 +36,11 @@ msgDist = MessageDistributor(config)
 receivingSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 receivingSocket.bind((masterConfig["MessageDistributorIP"], masterConfig["PortNumber"]["MessageDistributor"]))
 
+rawBsmLogging = config["raw_bsm_logging"]
+if rawBsmLogging == True:
+    logfile = open(("rawBsmLog_" + ('{:%m%d%Y_%H%M%S}'.format(datetime.datetime.now())) + ".csv"), 'w')
+    logfile.write("timestamp,secMark,temporaryId,latitude,longitude,elevation,speed,heading,type,length,width\n")
+
 while True:
     data, addr = receivingSocket.recvfrom(4096)
     msg = json.loads(data.decode())
@@ -41,9 +48,24 @@ while True:
     messageType = msgDist.distributeMsgToInfrastructureAndGetType(msg)
     if messageType == "BSM":
         msgDist.distributeBsmToClients(msg)
+        if rawBsmLogging == True:
+            logfile.write(str(msg["Timestamp_posix"]) + "," +
+            str(msg["BasicVehicle"]["secMark_Second"]) + "," +
+            str(msg["BasicVehicle"]["temporaryID"]) + "," +
+            str(msg["BasicVehicle"]["position"]["latitude_DecimalDegree"]) + "," +
+            str(msg["BasicVehicle"]["position"]["longitude_DecimalDegree"]) + "," +
+            str(msg["BasicVehicle"]["position"]["elevation_Meter"]) + "," +
+            str(msg["BasicVehicle"]["speed_MeterPerSecond"]) + "," +
+            str(msg["BasicVehicle"]["heading_Degree"]) + "," +
+            str(msg["BasicVehicle"]["type"]) + "," +
+            str(msg["BasicVehicle"]["size"]["length_cm"]) + "," +
+            str(msg["BasicVehicle"]["size"]["width_cm"]) + "\n"
+            )
     elif messageType == "MAP":
         msgDist.distributeMapToClients(msg)
     elif messageType == "SSM":
         msgDist.distributeSsmToClients(msg)
 
+if rawBsmLogging == True:
+    logfile.close()
 receivingSocket.close()
