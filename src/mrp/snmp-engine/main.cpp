ed12cddd51ff21ce63b51b58c9ea6ab48198aaa5
nvaltekar <nvaltekar@email.arizona.edu>
2020-07-07 02:03:18 -0700

    completed in-code documentation of SnmpEngine

--- src/mrp/snmp-engine/main.cpp
+++ src/mrp/snmp-engine/main.cpp
@@ -1,10 +1,40 @@
+/***************************************************************************************
+
+ © 2019 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+
+***************************************************************************************
+main.cpp (SnmpEngine)
+Created by: Niraj Vasant Altekar
+University of Arizona   
+College of Engineering
+
+This code was developed under the supervision of Professor Larry Head
+in the Systems and Industrial Engineering Department.
+
+***************************************************************************************
+
+Description:
+------------
+This is a wrapper application for the SnmpEngine class. Once started, the applciation
+waits for the messages over UDP socket. The received message is parsed into a JSON 
+and its type is identified. Based on the message type, appropriate methods from the 
+SnmpEngine class are called to process and execute the SnmpGet or SnmpSet request
+in the target SNMP device.
+
+***************************************************************************************/
+
 # include <iostream>
 # include <fstream>
 # include "json/json.h"
 # include "UdpSocket.h"
 # include "SnmpEngine.h"
+
 int main()
 {
+    std::string setCustomMibsCommand = "export MIBS=ALL";
+    system(setCustomMibsCommand.c_str()); 
+
     // Read configuration items:
     Json::Value jsonObject_config;
     Json::Reader reader;
@@ -12,50 +42,76 @@ int main()
     std::string configJsonString((std::istreambuf_iterator<char>(configJson)), std::istreambuf_iterator<char>());
     reader.parse(configJsonString.c_str(), jsonObject_config);
     configJson.close();
-    
+
+    // Read the network config for the SnmpEngine applciation    
     std::string ascIp = jsonObject_config["SignalController"]["IpAddress"].asString();
     int ascNtcipPort = jsonObject_config["SignalController"]["NtcipPort"].asUInt();
 
+    // Open a UDP socket for receiving messages from other clients for SnmpSet or SnmpGet
     UdpSocket snmpEngineSocket(static_cast<short unsigned int>(jsonObject_config["PortNumber"]["SnmpEngine"].asInt()));
+    
+    // Variables to store the received message and its type
     char receiveBuffer[10240]{};
     std::string msgType;
+    
+    // Variables to store the JSON object and JSON strings to be received
     Json::Value receivedJson;
     std::string receivedOid{};
+
+    // Variables to store the JSON object and JSON strings to be sent
+    Json::Value sendingJson;
+    Json::FastWriter fastWriter;
+    std::string sendingJsonString{};    
+
+    // Variables to store the SNMP requests related information
     int value{};
     int snmpResponse{};
 
-    Json::Value sendingJson;
-    Json::FastWriter fastWriter;
-    std::string sendingJsonString{};
-
+    // Variables to store the network information of request senders - Used for SnmpGet requests.
     std::string senderIp{};
     short unsigned int senderPort{};
 
+    // Instantiate an object opf SnmpEngine class
     SnmpEngine snmp(ascIp, ascNtcipPort);
 
     while(true)
     {
         snmpEngineSocket.receiveData(receiveBuffer, sizeof(receiveBuffer));
         reader.parse(receiveBuffer, receivedJson);
+        
+        // Parse the message type and OID for set or get request
         msgType = receivedJson["MsgType"].asString();
         receivedOid = receivedJson["OID"].asString();
+
         if(msgType=="SnmpSetRequest")
         {
             value = receivedJson["Value"].asInt();
+            
+            // Process the SNMP set request for given OID and given value
             snmpResponse = snmp.processSnmpRequest("set", receivedOid, value);
-
         }
+
         else if(msgType=="SnmpGetRequest")
         {
-            value=0;
-            snmpResponse = snmp.processSnmpRequest("get", receivedOid, value);
-            
+            // Stored the netowork information of the requestor so that the
+            // values received by SnmpGet call can be returned back
             senderIp = snmpEngineSocket.getSenderIP();
             senderPort = snmpEngineSocket.getSenderPort();
+
+            // SnmpGet request do not require any value to be provided initially
+            // This value is filled after getting the response from the target SNMP device
+            value=0;
+
+            // Process the SnmpGet request
+            snmpResponse = snmp.processSnmpRequest("get", receivedOid, value);
+            
+            // Formulate the JSOn string to send back to the requestor
             sendingJson["MsgType"] = "SnmpGetResponse";
             sendingJson["OID"] = receivedOid;
             sendingJson["Value"] = snmpResponse;
             sendingJsonString = fastWriter.write(sendingJson);
+
+            // Send the response to the requestor
             snmpEngineSocket.sendData(senderIp, senderPort, sendingJsonString);
         }
     }
