ad7eb611b2e7d4a8dd3634ca1f96b54ca0e2f005
nvaltekar <nvaltekar@email.arizona.edu>
2020-07-09 08:01:29 +0530

    added basic license headers

--- src/server/intersection-data-collector/src/video-data-collector.py
+++ src/server/intersection-data-collector/src/video-data-collector.py
@@ -1,3 +1,21 @@
+'''
+***************************************************************************************
+
+ © 2019 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+
+***************************************************************************************
+
+  video-data-collector.py  
+  Created by: Niraj Vasant Altekar
+  University of Arizona   
+  College of Engineering
+
+  This code was developed under the supervision of Professor Larry Head
+  in the Systems and Industrial Engineering Department.
+
+***************************************************************************************
+'''
 import os
 import time
 import datetime
