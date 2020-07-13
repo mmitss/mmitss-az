12e53254fe02c1fdf2ad792224216a53f37268a9
Debashis Das <debashisdas@email.arizona.edu>
2020-07-13 03:14:45 -0700

    updated mmitss field deployment documents

--- scripts/mmitss_launch_vsp.sh
+++
@@ -1,4 +0,0 @@
-read -p "Full absolute path of MMITSS configuration directory: " config_path
-read -p "Name of container image on the Dockerhub : " container_image
-
-docker run -v $config_path:/nojournal --network host --name vsp_container $container_image > /dev/null 2>&1 &
