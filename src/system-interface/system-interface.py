952733615c20ea0ace4889b5dbab7d551e36a693
skeatonUA <34190951+skeatonUA@users.noreply.github.com>
2020-09-08 11:58:39 -0700

    Removed extra files
    Removed extra files

--- src/system-interface/system-interface.py
+++ src/system-interface/system-interface.py
@@ -1,34 +1,56 @@
+"""
+***************************************************************************************
+ © 2020 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+***************************************************************************************
+system-interface.py
+Created by: Sherilyn Keaton
+University of Arizona   
+College of Engineering
+This code was developed under the supervision of Professor Larry Head
+in the Systems and Industrial Engineering Department.
+***************************************************************************************
+Description:
+------------
+This is a web-based Python Flask application that has the following functionality:
+    * MMITSS Component Process Control and Status Reporting Console
+    * MMITSS Configuration Viewer and Editor
+***************************************************************************************
+"""
+
 from flask import Flask, render_template, request, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 from wtforms.validators import *
 from flask_bootstrap import Bootstrap
 
+# Initialize application
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = '%sq72f#8c$seryfl#2h'
 
-# index
+# Index (Homepage)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# control console (remote)
+# Process Control Console (remote)
 @app.route('/console')
 def console():
     return render_template('console.html')
 
-# control console (local)
+# Process Control Console (local)
 @app.route('/local_console')
 def local_console():
     import json
 
     with open('static/json/mmitss-phase3-master-config.json') as json_file:
         data = json.load(json_file)
-        intersectionName = data['IntersectionName']
+        pageTitle = data['IntersectionName']
     
-    return render_template('local_console.html', intersectionName=intersectionName)
+    return render_template('local_console.html', pageTitle=pageTitle)
 
+# Configuration Viewer / Editor combined form
 class ConfigurationForm(FlaskForm):
     hostIp = StringField(validators=[ip_address()])
     sourceDsrcDeviceIp = StringField(validators=[ip_address()])
@@ -39,6 +61,7 @@ class ConfigurationForm(FlaskForm):
     hmiControllerIP = StringField()
     messageDistributorIP = StringField()
 
+# System Configuration data object
 class SysConfig:
     def __init__(self, data):
         self.hostIp = data['HostIp']
@@ -62,7 +85,6 @@ def prepareJSONData(data, form):
 
 # configuration viewer / editor
 @app.route('/configuration/', methods = ['GET', 'POST'])
-@app.route('/configuration/<ip_address>')
 def configuration():
     import json
 
@@ -70,6 +92,7 @@ def configuration():
         data = json.load(json_file)
 
     sysConfig = SysConfig(data)    
+    pageTitle = data['IntersectionName']
     form = ConfigurationForm(obj=sysConfig)
 
     #if request.method == 'POST' and form.validate():
@@ -78,20 +101,11 @@ def configuration():
         with open('static/json/mmitss-phase3-master-config.json', 'w') as json_file:
             prepareJSONData(data, form)
             dataResult = json.dump(data, json_file) 
-            #user = User(form.username.data, form.email.data,
-            #            form.password.data)
-            #db_session.add(user)
             flash('Configuration Updated')  
     
-    return render_template('configuration.html', data=data, form=form)
+    return render_template('configuration.html', pageTitle=pageTitle, form=form)
 
-    # Serializing json  
-    #with open('mmitss-phase3-master-config.json', 'w') as json_file:
-    #sys_config.hostIPtext = "10.10.10.10"
-    #data['HostIp'] = sys_config.hostIPtext
-    #data = json.dump(data, son_file) 
-
-# page not found 
+ # page not found 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
