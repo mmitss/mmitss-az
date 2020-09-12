7261dfeeb59c72eaa892bd588bb89f65276e56d9
skeatonUA <34190951+skeatonUA@users.noreply.github.com>
2020-09-12 08:44:09 -0700

    Added New Fields for Release
    Added New Fields for Release

--- src/system-interface/system-interface.py
+++ src/system-interface/system-interface.py
@@ -64,6 +64,9 @@ class ConfigurationForm(FlaskForm):
     vehicleType = StringField('Vehicle Type')
     logging = StringField('Logging')
     srmTimedOutTime = StringField('SRM Timed Out Time')
+    scheduleExecutionBuffer = StringField('Schedule Execution Buffer')
+    systemPerformanceTimeInterval = StringField('System Performance Time Interval')
+    applicationPlatform = StringField('Application Platform')
     portNumberMTMessageSender = StringField('Port Number: Message Transceiver / Message Sender')
     portNumberMTMessageReceiver = StringField('Port Number: Message Transceiver / Message Receiver')
     portNumberMTMessageEncoder = StringField('Port Number: Message Transceiver / Message Encoder')
@@ -90,6 +93,11 @@ class ConfigurationForm(FlaskForm):
     portNumberSnmpEngine = StringField('Port Number: SNMP Engine')
     portNumberSnmpEngineInterface = StringField('Port Number: SNMP Engine Interface')
     portNumberPriorityRequestGeneratorServer = StringField('Port Number: Priority Request Generator Server')
+    portNumberTrajectoryAware_MapEngineInterface = StringField('Port Number: Trajectory Aware Map Engine Interface')
+    portNumberMapEngine = StringField('Port Number: Map Engine')
+    portNumberPriorityLightSirenStatusManager = StringField('Port Number: Light Siren Status Manager')
+    portNumberPeerToPeerPriority = StringField('Port Number: Peer To Peer Priority')
+    portNumberSystemPerformanceDataCollector = StringField('Port Number: System Performance Data Collector')
     psidMap = StringField('PSID: Map')
     psidSPaT = StringField('PSID: SPaT')    
     psidRSM = StringField('PSID: RSM')    
@@ -104,6 +112,18 @@ class ConfigurationForm(FlaskForm):
     msgIdSSMLower = StringField('Msg ID: SSM Lower')
     msgIdSSMUpper = StringField('Msg ID: SSM Upper')
     msgIdBSM = StringField('Msg ID: BSM')
+    txChannelMap = StringField('Tx Channel: Map')
+    txChannelSPaT = StringField('Tx Channel: SPaT')    
+    txChannelRSM = StringField('Tx Channel: RSM')    
+    txChannelSRM = StringField('Tx Channel: SRM')    
+    txChannelSSM = StringField('Tx Channel: SSM')    
+    txChannelBSM = StringField('Tx Channel: BSM')
+    txModeMap = StringField('Tx Mode: Map')
+    txModeSPaT = StringField('Tx Mode: SPaT')    
+    txModeRSM = StringField('Tx Mode: RSM')    
+    txModeSRM = StringField('Tx Mode: SRM')    
+    txModeSSM = StringField('Tx Mode: SSM')    
+    txModeBSM = StringField('Tx Mode: BSM')
     signalControllerIP = StringField('Signal Controller IP Address')
     signalControllerNTCIPPort = StringField('Signal Controller NTCIP Address')
     signalControllerUpdateInterval = StringField('Signal Controller Timing Plan Update Interval')
@@ -120,6 +140,10 @@ class ConfigurationForm(FlaskForm):
     signalControllerPermissiveEnabled3 = StringField('Signal Controller Permissive Enabled 3')
     signalControllerPermissiveEnabled5 = StringField('Signal Controller Permissive Enabled 5')
     signalControllerPermissiveEnabled7 = StringField('Signal Controller Permissive Enabled 7')
+    intersectionReferencePointLatitudeDecimalDegree = StringField('Intersection Reference Point Latitude Decimal Degree')
+    intersectionReferencePointLongitudeDecimalDegree = StringField('Intersection Reference Point Longitude Decimal Degree')
+    intersectionReferencePointElevationMeter = StringField('Intersection Reference Point Elevation Meter')
+    
 
 # System Configuration data object
 class SysConfig:
@@ -136,6 +160,9 @@ class SysConfig:
         self.vehicleType = data['VehicleType']
         self.logging = data['Logging']
         self.srmTimedOutTime = data['SRMTimedOutTime']
+        self.scheduleExecutionBuffer = data['ScheduleExecutionBuffer']
+        self.systemPerformanceTimeInterval = data['SystemPerformanceTimeInterval']
+        self.applicationPlatform = data['ApplicationPlatform']
         self.portNumberMTMessageSender = data['PortNumber']['MessageTransceiver']['MessageSender']
         self.portNumberMTMessageReceiver = data['PortNumber']['MessageTransceiver']['MessageReceiver']
         self.portNumberMTMessageEncoder = data['PortNumber']['MessageTransceiver']['MessageEncoder']
@@ -150,6 +177,7 @@ class SysConfig:
         self.portNumberPriorityRequestGenerator = data['PortNumber']['PriorityRequestGenerator']
         self.portNumberTrafficControllerInterface = data['PortNumber']['TrafficControllerInterface']
         self.portNumberTrafficControllerCurrPhaseListener = data['PortNumber']['TrafficControllerCurrPhaseListener']
+        self.portNumberTrafficControllerTimingPlanSender = data['PortNumber']['TrafficControllerTimingPlanSender']
         self.portNumberPerformanceObserver = data['PortNumber']['PerformanceObserver']
         self.portNumberHMIController = data['PortNumber']['HMIController']
         self.portNumberPrioritySolverToTCIInterface = data['PortNumber']['PrioritySolverToTCIInterface']
@@ -161,6 +189,11 @@ class SysConfig:
         self.portNumberSnmpEngine = data['PortNumber']['SnmpEngine']
         self.portNumberSnmpEngineInterface = data['PortNumber']['SnmpEngineInterface']
         self.portNumberPriorityRequestGeneratorServer = data['PortNumber']['PriorityRequestGeneratorServer']
+        self.portNumberTrajectoryAware_MapEngineInterface = data['PortNumber']['TrajectoryAware_MapEngineInterface']
+        self.portNumberMapEngine = data['PortNumber']['MapEngine']
+        self.portNumberLightSirenStatusManager = data['PortNumber']['LightSirenStatusManager']
+        self.portNumberPeerToPeerPriority = data['PortNumber']['PeerToPeerPriority']
+        self.portNumberSystemPerformanceDataCollector = data['PortNumber']['SystemPerformanceDataCollector']
         self.psidMap = data['psid']['map']
         self.psidSPaT = data['psid']['spat']
         self.psidRSM = data['psid']['rsm']
@@ -175,6 +208,18 @@ class SysConfig:
         self.msgIdSSMLower = data['msgId']['ssm_lower']
         self.msgIdSSMUpper = data['msgId']['ssm_upper']
         self.msgIdBSM = data['msgId']['bsm']
+        self.txChannelMap = data['TxChannel']['map']
+        self.txChannelSPaT = data['TxChannel']['spat']
+        self.txChannelRSM = data['TxChannel']['rsm']
+        self.txChannelSRM = data['TxChannel']['srm']
+        self.txChannelSSM = data['TxChannel']['ssm']
+        self.txChannelBSM = data['TxChannel']['bsm']
+        self.txModeMap = data['TxMode']['map']
+        self.txModeSPaT = data['TxMode']['spat']
+        self.txModeRSM = data['TxMode']['rsm']
+        self.txModeSRM = data['TxMode']['srm']
+        self.txModeSSM = data['TxMode']['ssm']
+        self.txModeBSM = data['TxMode']['bsm']
         self.signalControllerIP = data['SignalController']['IpAddress']
         self.signalControllerNTCIPPort = data['SignalController']['NtcipPort']
         self.signalControllerUpdateInterval = data['SignalController']['TimingPlanUpdateInterval_sec']
@@ -191,6 +236,9 @@ class SysConfig:
         self.signalControllerPermissiveEnabled3 = data['SignalController']['PermissiveEnabled']['3']
         self.signalControllerPermissiveEnabled5 = data['SignalController']['PermissiveEnabled']['5']
         self.signalControllerPermissiveEnabled7 = data['SignalController']['PermissiveEnabled']['7']
+        self.intersectionReferencePointLatitudeDecimalDegree = data['IntersectionReferencePoint']['Latitude_DecimalDegree']
+        self.intersectionReferencePointLongitudeDecimalDegree = data['IntersectionReferencePoint']['Longitude_DecimalDegree']
+        self.intersectionReferencePointElevationMeter = data['IntersectionReferencePoint']['Elevation_Meter']
 
 
 
@@ -207,6 +255,9 @@ def prepareJSONData(data, form):
     data['VehicleType']= form.vehicleType.data
     data['Logging']= form.logging.data
     data['SRMTimedOutTime']= form.srmTimedOutTime.data
+    data['ScheduleExecutionBuffer']= form.scheduleExecutionBuffer.data
+    data['SystemPerformanceTimeInterval']= form.systemPerformanceTimeInterval.data
+    data['ApplicationPlatform']= form.applicationPlatform.data
     data['PortNumber']['MessageTransceiver']['MessageSender']= form.portNumberMTMessageSender.data
     data['PortNumber']['MessageTransceiver']['MessageReceiver']= form.portNumberMTMessageReceiver.data
     data['PortNumber']['MessageTransceiver']['MessageEncoder']= form.portNumberMTMessageEncoder.data
@@ -221,6 +272,7 @@ def prepareJSONData(data, form):
     data['PortNumber']['PriorityRequestGenerator']    = form.portNumberPriorityRequestGenerator.data
     data['PortNumber']['TrafficControllerInterface']    = form.portNumberTrafficControllerInterface.data
     data['PortNumber']['TrafficControllerCurrPhaseListener']    = form.portNumberTrafficControllerCurrPhaseListener.data
+    data['PortNumber']['TrafficControllerTimingPlanSender']    = form.portNumberTrafficControllerTimingPlanSender.data
     data['PortNumber']['PerformanceObserver']    = form.portNumberPerformanceObserver.data
     data['PortNumber']['HMIController']    = form.portNumberHMIController.data
     data['PortNumber']['PrioritySolverToTCIInterface']    = form.portNumberPrioritySolverToTCIInterface.data
@@ -232,7 +284,12 @@ def prepareJSONData(data, form):
     data['PortNumber']['SnmpEngine']    = form.portNumberSnmpEngine.data
     data['PortNumber']['SnmpEngineInterface']    = form.portNumberSnmpEngineInterface.data
     data['PortNumber']['PriorityRequestGeneratorServer']    = form.portNumberPriorityRequestGeneratorServer.data
-    data['psid']['map']    = form.psidMap.data
+    data['PortNumber']['TrajectoryAware_MapEngineInterface']    = form.portNumberTrajectoryAware_MapEngineInterface.data
+    data['PortNumber']['MapEngine']    = form.portNumberMapEngine.data
+    data['PortNumber']['LightSirenStatusManager']    = form.portNumberLightSirenStatusManager.data
+    data['PortNumber']['PeerToPeerPriority']    = form.portNumberPeerToPeerPriority.data
+    data['PortNumber']['SystemPerformanceDataCollector']    = form.portNumberSystemPerformanceDataCollector.data
+    data['psid']['map']    = form.psidMap.data    
     data['psid']['spat']    = form.psidSPaT.data
     data['psid']['rsm']    = form.psidRSM.data
     data['psid']['srm']    = form.psidSRM.data
@@ -246,6 +303,18 @@ def prepareJSONData(data, form):
     data['msgId']['ssm_lower']    = form.msgIdSSMLower.data
     data['msgId']['ssm_upper']    = form.msgIdSSMUpper.data
     data['msgId']['bsm']    = form.msgIdBSM.data
+    data['TxChannel']['map']    = form.txChannelMap.data    
+    data['TxChannel']['spat']    = form.txChannelSPaT.data
+    data['TxChannel']['rsm']    = form.txChannelRSM.data
+    data['TxChannel']['srm']    = form.txChannelSRM.data
+    data['TxChannel']['ssm']    = form.txChannelSSM.data
+    data['TxChannel']['bsm']    = form.txChannelBSM.data
+    data['TxMode']['map']    = form.txModeMap.data    
+    data['TxMode']['spat']    = form.txModeSPaT.data
+    data['TxMode']['rsm']    = form.txModeRSM.data
+    data['TxMode']['srm']    = form.txModeSRM.data
+    data['TxMode']['ssm']    = form.txModeSSM.data
+    data['TxMode']['bsm']    = form.txModeBSM.data
     data['SignalController']['IpAddress']    = form.signalControllerIP.data
     data['SignalController']['NtcipPort']    = form.signalControllerNTCIPPort.data
     data['SignalController']['TimingPlanUpdateInterval_sec']    = form.signalControllerUpdateInterval.data
@@ -262,6 +331,9 @@ def prepareJSONData(data, form):
     data['SignalController']['PermissiveEnabled'] ['3']   = form.signalControllerPermissiveEnabled3.data
     data['SignalController']['PermissiveEnabled'] ['5']   = form.signalControllerPermissiveEnabled5.data
     data['SignalController']['PermissiveEnabled'] ['7']   = form.signalControllerPermissiveEnabled7.data
+    data['IntersectionReferencePoint']['Latitude_DecimalDegree']    = form.intersectionReferencePointLatitudeDecimalDegree.data
+    data['IntersectionReferencePoint']['Longitude_DecimalDegree']   = form.intersectionReferencePointLongitudeDecimalDegree.data
+    data['IntersectionReferencePoint']['Elevation_Meter']           = form.intersectionReferencePointElevationMeter.data
 
 
 # configuration viewer / editor
@@ -270,6 +342,8 @@ def configuration():
     import json
 
     with open('/nojournal/bin/mmitss-phase3-master-config.json') as json_file:
+    #test location
+    #with open('static/json/mmitss-phase3-master-config.json') as json_file:
         data = json.load(json_file)
 
     sysConfig = SysConfig(data)    
@@ -279,7 +353,9 @@ def configuration():
     #if request.method == 'POST' and form.validate():
     if request.method == 'POST':
         # Serialize the edited data
-        with open('static/json/mmitss-phase3-master-config.json', 'w') as json_file:
+        with open('/nojournal/bin/mmitss-phase3-master-config.json', 'w') as json_file:
+        #test location
+        #with open('static/json/mmitss-phase3-master-config.json', 'w') as json_file:
             prepareJSONData(data, form)
             dataResult = json.dump(data, json_file) 
             flash('Configuration Updated')  
