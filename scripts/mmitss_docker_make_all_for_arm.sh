0f4dbb5ed37cb84cd74ea78ee56095485e04a885
pearson10m <pearson10m@gmail.com>
2020-09-13 20:30:06 +0000

    Preliminary work on pyinstalling SystemInterface

--- scripts/mmitss_docker_make_all_for_arm.sh
+++ scripts/mmitss_docker_make_all_for_arm.sh
@@ -294,3 +294,23 @@ rm -r __pycache__ &> /dev/null
 cd - &> /dev/null
 sleep 1s
 #######################################################################################
+
+#######################################################################################
+echo "Building System Interface..."
+cd ./../src/system-interface
+# Clean the folder and build for linux.
+pyinstaller --add-data "templates:templates" --add-data "static:static" --additional-hooks-dir=. --windowed system-interface.py &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/system-interface  ../../../bin/SystemInterface/arm/M_SystemInterface
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
