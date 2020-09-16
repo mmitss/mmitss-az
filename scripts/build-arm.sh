be7fe456b5a7facb4f2a0f2c9390b4cfd4b44508
debipe20 <debashisdas@emali.arizona.edu>
2020-09-16 13:28:39 -0700

    modified the supervisord.conf and Dockerfiles for the vsp and cleaned up the branch

--- scripts/build-arm.sh
+++ scripts/build-arm.sh
@@ -255,30 +255,6 @@ cd - &> /dev/null
 sleep 1s
 #######################################################################################
 
-
-
-#######################################################################################
-echo "Building Light Siren Status Manager..."
-cd ./../src/vsp/light-siren-status-manager
-# Clean the folder and build for linux.
-pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed M_LightSirenStatusManager.py  &> /dev/null
-# Indicate Success/Failure of the build
-if [ "$?" -eq "0" ]; then
-    mv dist/M_LightSirenStatusManager  ../../../bin/LightSirenStatusManager/arm
-	echo -e "${green}Successful${nocolor}"
-else
-	echo -e "${red}Failed${nocolor}"
-fi
-# Remove the .o files to keep the folders clean
-rm -r build dist *.spec &> /dev/null
-rm -r __pycache__ &> /dev/null
-# Return back to original directory to go over the process again for another one
-cd - &> /dev/null
-sleep 1s
-#######################################################################################
-
-
-
 #######################################################################################
 echo "Building System Interface..."
 cd ./../src/system-interface
