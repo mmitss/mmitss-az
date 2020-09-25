672bec623b68b2538e2bfde3deeafffc94444ccd
nvaltekar <nvaltekar@email.arizona.edu>
2020-09-25 08:48:52 -0700

    initialized ftp-server

---
+++ src/server/v2x-data/src/v2x-data-ftp-server.py
@@ -0,0 +1,38 @@
+import os
+import json
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+# Read the configuration file
+with open("../config/v2x-data-ftp-server-config.json") as configFile:
+    config = json.load(configFile)
+
+
+# Instantiate a dummy authorizer for managing 'virtual' users
+authorizer = DummyAuthorizer()
+
+for client in config["clients"]:
+    authorizer.add_user(username=client["name"],
+                        password='mmitss123',
+                        homedir=client["directory"], 
+                        perm='elradfmwMT',
+                        msg_login=(client["name"] + "logged in"),
+                        msg_quit=(client["name"] + "logged out"))
+
+# Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+
+# Instantiate FTP server class to listen at specified address
+address = (config["ftpHost"], config["ftpPort"])
+server = FTPServer(address, handler)
+
+# set a limit for connections
+server.max_cons = 256
+server.max_cons_per_ip = 5
+
+# start ftp server
+server.serve_forever()
+
