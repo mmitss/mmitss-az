f7997b796bb94137fb28bf152aaf044c1e639889
nvaltekar <nvaltekar@email.arizona.edu>
2020-09-27 15:42:05 -0700

    merged vsp/mrp build scripts

--- scripts/build-arm.sh
+++ scripts/build-arm.sh
@@ -213,5 +213,68 @@ cd - &> /dev/null
 sleep 1s
 #######################################################################################
 
+################################# VEHICLE APPLICATIONS ################################
+
+#######################################################################################
+echo "Building Host BSM Decoder..."
+cd ./../src/common/MsgTransceiver/MsgDecoder/HostBsmDecoder
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_HostBsmDecoder ../../../../../bin/HostBsmDecoder/arm
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Priority Request Generator..."
+cd ./../src/vsp/priority-request-generator
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_PriorityRequestGenerator ../../../bin/PriorityRequestGenerator/arm
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Light Siren Status Manager..."
+cd ./../src/vsp/light-siren-status-manager
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed light-siren-status-manager.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/light-siren-status-manager  ../../../bin/LightSirenStatusManager/arm/M_LightSirenStatusManager
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+
 
 
