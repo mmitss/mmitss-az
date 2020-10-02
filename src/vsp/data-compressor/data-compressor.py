5bd686fea317fa660b5fcc81370f0670b0248b73
nvaltekar <nvaltekar@email.arizona.edu>
2020-10-02 13:20:33 -0700

    made a final commit before field test

--- src/vsp/data-compressor/data-compressor.py
+++ src/vsp/data-compressor/data-compressor.py
@@ -33,16 +33,17 @@ while len (directories) > 0:
         shutil.rmtree((path + "/" + directory))
         directories = list(os.walk(path))[0][1]
 
-list_of_files = os.listdir("/nojournal/bin/v2x-data/archive")
-full_path = ["/nojournal/bin/v2x-data/archive/{0}".format(x) for x in list_of_files]
+list_of_files = os.listdir(path)
+full_path = [path + "/{0}".format(x) for x in list_of_files]
 
-blockSize = os.statvfs("/nojournal").f_frsize
-remainingDiskSpace_gb = os.statvfs("/nojournal")[4]*blockSize/(BYTES_PER_GB) 
+blockSize = os.statvfs(path).f_frsize
+remainingDiskSpace_gb = os.statvfs(path)[4]*blockSize/(BYTES_PER_GB) 
 
 while (remainingDiskSpace_gb) < MIN_DISK_SPACE_GB:
-    remainingDiskSpace_gb = os.statvfs("/nojournal")[4]*blockSize/(BYTES_PER_GB)
-    list_of_files = os.listdir("/nojournal/bin/v2x-data/archive")
-    full_path = ["/nojournal/bin/v2x-data/archive/{0}".format(x) for x in list_of_files]
+    print("Low disk space: removing old logs")
+    remainingDiskSpace_gb = os.statvfs(path)[4]*blockSize/(BYTES_PER_GB)
+    list_of_files = os.listdir(path)
+    full_path = [path + "/{0}".format(x) for x in list_of_files]
     if len(full_path) > 0:
         oldest_file = min(full_path, key=os.path.getctime)
         os.remove(oldest_file)
