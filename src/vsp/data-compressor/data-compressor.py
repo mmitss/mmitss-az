3e45a6d685d499c559bef63cac8cdf1a79c4206a
nvaltekar <nvaltekar@email.arizona.edu>
2020-10-02 13:07:06 -0700

    added a condition to check free size on device

--- src/vsp/data-compressor/data-compressor.py
+++ src/vsp/data-compressor/data-compressor.py
@@ -20,6 +20,9 @@ in the Systems and Industrial Engineering Department.
 import os
 import shutil
 
+MIN_DISK_SPACE_GB = 1
+BYTES_PER_GB = 1024*1024*1024
+
 path = "/nojournal/bin/v2x-data/archive"
 
 directories = list(os.walk(path))[0][1]
@@ -29,3 +32,19 @@ while len (directories) > 0:
         shutil.make_archive((path + "/" + directory), "zip", (path + "/" + directory))
         shutil.rmtree((path + "/" + directory))
         directories = list(os.walk(path))[0][1]
+
+list_of_files = os.listdir("/nojournal/bin/v2x-data/archive")
+full_path = ["/nojournal/bin/v2x-data/archive/{0}".format(x) for x in list_of_files]
+
+blockSize = os.statvfs("/nojournal").f_frsize
+remainingDiskSpace_gb = os.statvfs("/nojournal")[4]*blockSize/(BYTES_PER_GB) 
+
+while (remainingDiskSpace_gb) < MIN_DISK_SPACE_GB:
+    remainingDiskSpace_gb = os.statvfs("/nojournal")[4]*blockSize/(BYTES_PER_GB)
+    list_of_files = os.listdir("/nojournal/bin/v2x-data/archive")
+    full_path = ["/nojournal/bin/v2x-data/archive/{0}".format(x) for x in list_of_files]
+    if len(full_path) > 0:
+        oldest_file = min(full_path, key=os.path.getctime)
+        os.remove(oldest_file)
+    
+
