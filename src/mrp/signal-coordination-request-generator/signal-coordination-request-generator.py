35b386b20412090468b322343c31e33019888b0f
debipe20 <debashisdas@email.arizona.edu>
2020-11-23 00:11:51 +0600

    updated the logic for clearing the coordination parameter dictionary

--- src/mrp/signal-coordination-request-generator/signal-coordination-request-generator.py
+++ src/mrp/signal-coordination-request-generator/signal-coordination-request-generator.py
@@ -63,8 +63,6 @@ def main():
     coordinationRequestManager = CoordinationRequestManager(config)
     
     while True:
-        coordinationRequestManager.getMinuteOfYear()
-        coordinationRequestManager.getMsOfMinute()
         # Check if it is required to obtain active coordination plan or not
         # Send the split data to the PRSolver
         if bool(coordinationPlanManager.checkActiveCoordinationPlan()):
