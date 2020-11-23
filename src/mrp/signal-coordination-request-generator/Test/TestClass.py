4a2a877a0fedd14ff35978b8759864ededa771d6
debipe20 <debashisdas@email.arizona.edu>
2020-11-23 20:26:26 +0600

    cleaned up the code

--- src/mrp/signal-coordination-request-generator/Test/TestClass.py
+++
@@ -1,6 +0,0 @@
-class TestClass(object):
-    def __init__(self, data):
-        self.data = data
-    def print(self):
-        print (self.data['name']) # Oscar 
-        print (self.data['age'] + 10) # 42  
