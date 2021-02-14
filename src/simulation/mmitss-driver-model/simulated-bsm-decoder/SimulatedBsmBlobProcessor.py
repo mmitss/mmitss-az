2ebde0d597df2df3e14a861bdca28f0cb4f91c4c
nvaltekar <nvaltekar@email.arizona.edu>
2021-02-13 21:37:09 -0700

    added SimulatedBsmBlobProcessor application in the source tree

--- src/simulation/mmitss-driver-model/simulated-bsm-decoder/SimulatedBsmBlobProcessor.py
+++ src/simulation/mmitss-driver-model/simulated-bsm-decoder/SimulatedBsmBlobProcessor.py
@@ -1,7 +1,26 @@
+"""
+**********************************************************************************
+
+ © 2021 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+
+**********************************************************************************
+
+  SimulatedBsmBlobProcessor.py  
+  Created by: Niraj Vasant Altekar
+  University of Arizona   
+  College of Engineering
+
+  This code was developed under the supervision of Professor Larry Head
+  in the Systems and Industrial Engineering Department.
+
+**********************************************************************************
+"""
 import bitstring
 import json
 import time, datetime
 
+# Define constant multipliers:
 MULTIPLIER_LATITUDE     =   10000000
 MULTIPLIER_LONGITUDE    =   10000000
 MULTIPLIER_ELEVATION    =   100
@@ -10,13 +29,34 @@ MULTIPLIER_HEADING      =   100
 MULTIPLIER_LENGTH       =   10
 MULTIPLIER_WIDTH        =   10
 
+# Define dictioary of vehicle types:
+VEH_TYPE_DICT           = {0:"unknown", 2: "EmergencyVehicle", 4: "Car", 6: "Transit", 9: "Truck"}
+
 class SimulatedBsmBlobProcessor:
+    """
+    provides methods to: 
+    (1) pack the BSM information (either from individual variables or JSON string) into a blob
+    (2) unpack the received blob and develop BSM json string
+    """
     def __init__(self):
         self.blobStructure = 'uint:8, uint:64, uint:16, int:32, int:32, int:32, uint:16, uint:16, uint:16, uint:16, uint:8'
 
-    def pack_variables_to_blob(self, msgCount, temporaryId, secMark, latitude_DecimalDegree, longitude_DecimalDegree,
-                    elevation_Meter, speed_MeterPerSecond, heading_Degree, length_cm, width_cm, vehicle_Type):
-
+    def pack_variables_to_blob(self, 
+                                msgCount:int, 
+                                temporaryId:int, 
+                                secMark:int, 
+                                latitude_DecimalDegree:float, 
+                                longitude_DecimalDegree:float,
+                                elevation_Meter:float, 
+                                speed_MeterPerSecond:float, 
+                                heading_Degree:float, 
+                                length_cm:float, 
+                                width_cm:float, 
+                                vehicle_Type:(int or str)) -> bytes:
+        """
+        packs the information received in arguments into a blob and returns the blob
+        """
+        # Multiply with appropriate multipliers to convert floats into ints without loosing the information:
         latitude_DecimalDegree = latitude_DecimalDegree * MULTIPLIER_LATITUDE
         longitude_DecimalDegree = longitude_DecimalDegree * MULTIPLIER_LONGITUDE
         elevation_Meter = elevation_Meter * MULTIPLIER_ELEVATION
@@ -24,14 +64,29 @@ class SimulatedBsmBlobProcessor:
         heading_Degree = heading_Degree *  MULTIPLIER_HEADING
         length_cm = length_cm * MULTIPLIER_LENGTH
         width_cm = width_cm * MULTIPLIER_WIDTH
-        
+
+        # Validate the vehicle type:
+        if type(vehicle_Type) == str and vehicle_Type in VEH_TYPE_DICT.values():                
+            vehicle_Type = dict(zip(VEH_TYPE_DICT.values(),VEH_TYPE_DICT.keys()))[vehicle_Type]
+        elif type(vehicle_Type) == int and vehicle_Type in VEH_TYPE_DICT.keys():
+            vehicle_Type = vehicle_Type
+        else: vehicle_Type = 0
+
+        # Pack the information into a blob:
         blob = bitstring.pack(self.blobStructure, msgCount, temporaryId, secMark, latitude_DecimalDegree, longitude_DecimalDegree,
                         elevation_Meter, speed_MeterPerSecond, heading_Degree, length_cm, width_cm, vehicle_Type).bytes
+        
+        # Return the blob:
         return blob
     
-    def pack_json_to_blob(self, bsmJson:str):           
-        
+    def pack_json_to_blob(self, bsmJson:str) -> (bytes or bool):
+        """
+        packs the information in the BSM Json string received in the arguments into a blob and returns the blob.
+        If JSON string having MsgType other than BSM, then returns False.
+        """         
+        # Load the received JSON string into a JSON object:
         bsmJson = json.loads(bsmJson)
+
         if bsmJson["MsgType"] == "BSM":
             msgCount = 0
             temporaryId = bsmJson["BasicVehicle"]["temporaryID"]
@@ -43,17 +98,35 @@ class SimulatedBsmBlobProcessor:
             heading_Degree = bsmJson["BasicVehicle"]["heading_Degree"] * MULTIPLIER_HEADING
             length_cm = bsmJson["BasicVehicle"]["size"]["length_cm"] * MULTIPLIER_LENGTH
             width_cm = bsmJson["BasicVehicle"]["size"]["width_cm"] * MULTIPLIER_WIDTH
-            vehicle_Type = bsmJson["BasicVehicle"]["type"]        
+            vehicle_Type = bsmJson["BasicVehicle"]["type"]
+            
+            # Validate the vehicle type:
+            if type(vehicle_Type) == str and vehicle_Type in VEH_TYPE_DICT.values():                
+                vehicle_Type = dict(zip(VEH_TYPE_DICT.values(),VEH_TYPE_DICT.keys()))[vehicle_Type]
+            elif type(vehicle_Type) == int and vehicle_Type in VEH_TYPE_DICT.keys():
+                vehicle_Type = vehicle_Type
+            else: vehicle_Type = 0
+
+            # Pack the information into a blob:
             blob = bitstring.pack(self.blobStructure, msgCount, temporaryId, secMark, latitude_DecimalDegree, longitude_DecimalDegree,
                                     elevation_Meter, speed_MeterPerSecond, heading_Degree, length_cm, width_cm, vehicle_Type).bytes
             return blob
         else:
             return False
     
-    def unpack_blob_to_json(self, blob:bytes):
-
+    def unpack_blob_to_json(self, blob:bytes) -> str:
+        """
+        unpacks the BSM_Blob received from VISSIM and returns the JSON string filled with received values
+        """
+        # Unpack the blob:
         blob = bitstring.BitStream(blob).unpack(self.blobStructure)
 
+        # Validate the vehicle type and convert it into a string:
+        if blob[10] in VEH_TYPE_DICT.keys():
+            vehicle_Type = VEH_TYPE_DICT[blob[10]]
+        else: vehicle_Type = VEH_TYPE_DICT[0]
+        
+        # Formulate JSON object containing BSM information:
         bsmJson = {
                     "MsgType": "BSM",
                     "Timestamp_posix": time.time(),
@@ -65,7 +138,7 @@ class SimulatedBsmBlobProcessor:
                         "secMark_Second": blob[2],
                         "speed_MeterPerSecond": (blob[6] / MULTIPLIER_SPEED),
                         "heading_Degree": (blob[7] / MULTIPLIER_HEADING),
-                        "type": blob[10],
+                        "type": vehicle_Type,
                         "position":
                         {
                             "latitude_DecimalDegree": (blob[3] / MULTIPLIER_LATITUDE), 
@@ -79,7 +152,10 @@ class SimulatedBsmBlobProcessor:
                         }
                     }                                    
                 }
+        # Convert the JSON object into a JSON string:
         bsmJson = json.dumps(bsmJson)
+
+        # Return the JSON string:
         return bsmJson
 
 if __name__ == "__main__":
@@ -93,7 +169,7 @@ if __name__ == "__main__":
     heading_Degree          =   38.56 # uint16
     length_cm               =   421.1 # uint16
     width_cm                =   200.3 # uint16
-    vehicle_Type            =   2 # uint16
+    vehicle_Type            =   9 # uint16 ONLY VALID VALUES: 0, 2, 4, 6, 9
 
     processor = SimulatedBsmBlobProcessor()
 
--- src/simulation/mmitss-driver-model/simulated-bsm-decoder/simulated-bsm-blob-processor.py
+++ src/simulation/mmitss-driver-model/simulated-bsm-decoder/simulated-bsm-blob-processor.py
@@ -1,47 +1,52 @@
+"""
+**********************************************************************************
+
+ © 2021 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+
+**********************************************************************************
+
+  simulated-bsm-blob-processor.py  
+  Created by: Niraj Vasant Altekar
+  University of Arizona   
+  College of Engineering
+
+  This code was developed under the supervision of Professor Larry Head
+  in the Systems and Industrial Engineering Department.
+
+**********************************************************************************
+"""
+import json
 import socket
 import time, datetime
 from SimulatedBsmBlobProcessor import SimulatedBsmBlobProcessor
 
+# Load the information from the config file:
+with open("/nojournal/bin/mmitss-phase3-master-config.json", 'r') as configFile:
+    config = json.load(configFile)
+
+host = (config["HostIp"], config["PortNumber"]["HostBsmDecoder"])
+messageDistributor = (config["HostIp"], config["PortNumber"]["MessageDistributor"])
+
+# Create an object of SimulatedBsmBlobProcessor class:
 processor = SimulatedBsmBlobProcessor()
 
-fp = open("SimulatedBsmLog.csv", 'w')
-fp.write("log_timestamp_verbose,log_timestamp_posix,timestamp_verbose,timestamp_posix,"
-                                    + "temporaryId,secMark,latitude,longitude,elevation,speed,heading,type,length,width\n")
+# Create the socket and bind it to the appropriate address:
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("10.12.6.251"),5060)
+s.bind(host)
 
 while True:
+    # Receive blob data (from VISSIM)
     data, address = s.recvfrom(128)
-    bsmJson = processor.unpack_blob_to_json(data)
-    log_timestamp_verbose = str(datetime.datetime.now())
-    log_timestamp_posix = str(time.time())
-    timestamp_verbose = str(bsmJson["Timestamp_verbose"])
-    timestamp_posix = str(bsmJson["Timestamp_posix"])
-    temporaryId = str(bsmJson["BasicVehicle"]["temporaryID"])
-    secMark = str(bsmJson["BasicVehicle"]["secMark_Second"])
-    latitude = str(bsmJson["BasicVehicle"]["position"]["latitude_DecimalDegree"])
-    longitude = str(bsmJson["BasicVehicle"]["position"]["longitude_DecimalDegree"])
-    elevation = str(bsmJson["BasicVehicle"]["position"]["elevation_Meter"])
-    speed = str(bsmJson["BasicVehicle"]["speed_MeterPerSecond"])
-    heading = str(bsmJson["BasicVehicle"]["heading_Degree"])
-    vehType = str(bsmJson["BasicVehicle"]["type"])
-    length = str(bsmJson["BasicVehicle"]["size"]["length_cm"])
-    width = str(bsmJson["BasicVehicle"]["size"]["width_cm"])
-    csv = (log_timestamp_verbose + "," 
-                + log_timestamp_posix + "," 
-                + timestamp_verbose + "," 
-                + timestamp_posix + "," 
-                + temporaryId + "," 
-                + secMark + "," 
-                + latitude + "," 
-                + longitude + "," 
-                + elevation + "," 
-                + speed + "," 
-                + heading + "," 
-                + vehType + ","
-                + length + "," 
-                + width + "\n")
-    fp.write(csv)
-fp.close()
-socket.close()
+    
+    # Try to unpack the blob data
+    try:
+        bsmJson = processor.unpack_blob_to_json(data)
+        # Forward the unpacked data (JSON) to the message distributor
+        s.sendto(bsmJson.encode(), messageDistributor)
+    except: pass
+    
+# Close the socket before exiting
+s.close()
 
+    
