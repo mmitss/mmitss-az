729b51cae701f2333a05262a73aa1d2d9fcfa03a
nvaltekar <nvaltekar@email.arizona.edu>
2021-02-22 22:17:01 -0700

    added copyright headers

--- src/common/v2x-data-transfer/PushToServer.py
+++ src/common/v2x-data-transfer/PushToServer.py
@@ -1,3 +1,22 @@
+"""
+**********************************************************************************
+
+ © 2021 Arizona Board of Regents on behalf of the University of Arizona with rights
+       granted for USDOT OSADP distribution with the Apache 2.0 open source license.
+
+**********************************************************************************
+
+  PushToServer.py  
+  Created by: Niraj Vasant Altekar
+  University of Arizona   
+  College of Engineering
+
+  This code was developed under the supervision of Professor Larry Head
+  in the Systems and Industrial Engineering Department.
+
+**********************************************************************************
+"""
+
 # Import system libraries:
 import os
 import datetime
