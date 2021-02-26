e8be9d9cbbb40232311f0c5d4d07eed516f4ebad
nvaltekar <nvaltekar@email.arizona.edu>
2021-02-26 12:06:42 -0700

    renamed folders of configurations

---
+++ build/scripts/build-arm.sh
@@ -0,0 +1,401 @@
+#!/bin/bash
+#############################################################################################
+#                                                                                           
+# NOTICE:  Copyright 2018 Arizona Board of Regents on behalf of University of Arizona.      
+# All information, intellectual, and technical concepts contained herein is and shall       
+# remain the proprietary information of Arizona Board of Regents and may be covered         
+# by U.S. and Foreign Patents, and patents in process.  Dissemination of this information          
+# or reproduction of this material is strictly forbidden unless prior written permission    
+# is obtained from Arizona Board of Regents or University of Arizona.                       
+#                                                                                           
+# build-arm.sh                                                                     
+# Created by Niraj Altekar                                                                  
+# Transportation Research Institute                                                         
+# Systems and Industrial Engineering                                                        
+# The University of Arizona                                                                 
+#                                                                                           
+# This code was develop under the supervision of Professor Larry Head                       
+# in the Transportation Research Institute.                                                 
+#                                                                                           
+# Operational Description:                                                                   
+# This script builds all mmitss applications (vehicle, intersection, and common),
+# under the arm environment. The primary reason for such builds is development and testing.
+# This script can not be used in the x86 architecture based devices.                                                                                                  
+#############################################################################################
+
+# Define colors:
+red='\033[0;31m'
+green='\033[0;32m'
+nocolor='\033[0m'
+
+################################## COMMON APPLICATIONS ################################
+
+######################################################################################
+echo "Building Message Encoder..."
+cd ../../src/common/MsgTransceiver/MsgEncoder
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_MsgEncoder ../../../../build/bin/MsgEncoder/arm/M_MsgEncoder
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Wireless Message Decoder..."
+cd ../../src/common/MsgTransceiver/MsgDecoder/WirelessMsgDecoder
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_WirelessMsgDecoder ../../../../../build/bin/WirelessMsgDecoder/arm/M_WirelessMsgDecoder
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building V2X Data Collector..."
+cd ../../src/common/v2x-data-collector
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed v2x-data-collector-main.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/v2x-data-collector-main  ../../../build/bin/V2XDataCollector/arm/M_V2XDataCollector
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building System Interface..."
+cd ../../src/system-interface
+# Clean the folder and build for linux.
+pyinstaller --add-data "templates:templates" --add-data "static:static" --additional-hooks-dir=. --onefile --windowed system-interface.py &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/system-interface  ../../build/bin/SystemInterface/arm/M_SystemInterface
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+############################### INTERSECTION APPLICATIONS #############################
+
+#######################################################################################
+echo "Building Priority Request Server..."
+cd ../../src/mrp/priority-request-server
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_PriorityRequestServer ../../../build/bin/PriorityRequestServer/arm/M_PriorityRequestServer
+    echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Priority Solver..."
+cd ../../src/mrp/priority-request-solver
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_PriorityRequestSolver ../../../build/bin/PriorityRequestSolver/arm/M_PriorityRequestSolver
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+######################################################################################
+
+#######################################################################################
+echo "Building Snmp Engine..."
+cd ../../src/mrp/snmp-engine
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_SnmpEngine ../../../build/bin/SnmpEngine/arm/M_SnmpEngine
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+######################################################################################
+
+#######################################################################################
+echo "Building Traffic Controller Interface..."
+cd ../../src/mrp/traffic-controller-interface
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed traffic-controller-interface.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/traffic-controller-interface  ../../../build/bin/TrafficControllerInterface/arm/M_TrafficControllerInterface
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist __pychache__ *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Map Spat Broadcaster..."
+cd ../../src/mrp/map-spat-broadcaster
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed map-spat-broadcaster.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/map-spat-broadcaster  ../../../build/bin/MapSpatBroadcaster/arm/M_MapSpatBroadcaster
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Signal Coordination Request Generator..."
+cd ../../src/mrp/signal-coordination-request-generator
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed signal-coordination-request-generator.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/signal-coordination-request-generator  ../../../build/bin/SignalCoordinationRequestGenerator/arm/M_SignalCoordinationRequestGenerator
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building V2X Data Ftp Client..."
+cd ../../src/mrp/v2x-data-ftp-client
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed v2x-data-ftp-client-main.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/v2x-data-ftp-client-main  ../../../build/bin/V2XDataFtpClient/arm/M_V2XDataFtpClient
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+################################# VEHICLE APPLICATIONS ################################
+
+#######################################################################################
+echo "Building Host BSM Decoder..."
+cd ../../src/common/MsgTransceiver/MsgDecoder/HostBsmDecoder
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_HostBsmDecoder ../../../../../build/bin/HostBsmDecoder/arm/M_HostBsmDecoder
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Priority Request Generator..."
+cd ../../src/vsp/priority-request-generator
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_PriorityRequestGenerator ../../../build/bin/PriorityRequestGenerator/arm/M_PriorityRequestGenerator
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Light Siren Status Manager..."
+cd ../../src/vsp/light-siren-status-manager
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed light-siren-status-manager.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/light-siren-status-manager  ../../../build/bin/LightSirenStatusManager/arm/M_LightSirenStatusManager
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Data Compressor..."
+cd ../../src/vsp/data-compressor
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed data-compressor.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/data-compressor  ../../../build/bin/DataCompressor/arm/M_DataCompressor
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+################################### SIMULATION TOOLS ##################################
+
+#######################################################################################
+echo "Building Priority Request Generator Server..."
+cd ../../src/server/priority-request-generator-server
+# Clean the folder and build for linux.
+make clean &> /dev/null
+make linux ARM=1 &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv M_PriorityRequestGeneratorServer ../../../build/bin/PriorityRequestGeneratorServer/arm/M_PriorityRequestGeneratorServer
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm ./*.o &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Message Distributor..."
+cd ../../src/server/message-distributor
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed message-distributor.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/message-distributor  ../../../build/bin/MessageDistributor/arm/M_MessageDistributor
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+#######################################################################################
+
+#######################################################################################
+echo "Building Simulated BSM Blob Processor..."
+cd ../../src/simulation/simulated-bsm-blob-processor
+# Clean the folder and build for linux.
+pyinstaller --hidden-import=pkg_resources.py2_warn --onefile --windowed simulated-bsm-blob-processor.py  &> /dev/null
+# Indicate Success/Failure of the build
+if [ "$?" -eq "0" ]; then
+    mv dist/simulated-bsm-blob-processor  ../../../build/bin/SimulatedBsmBlobProcessor/arm/M_SimulatedBsmBlobProcessor
+	echo -e "${green}Successful${nocolor}"
+else
+	echo -e "${red}Failed${nocolor}"
+fi
+# Remove the .o files to keep the folders clean
+rm -r build dist *.spec &> /dev/null
+rm -r __pycache__ &> /dev/null
+# Return back to original directory to go over the process again for another one
+cd - &> /dev/null
+sleep 1s
+
+#######################################################################################
+
+
---
+++ build/scripts/lmmitss-initialize.sh
@@ -0,0 +1,107 @@
+#############################################################################################
+#                                                                                           #
+# NOTICE:  Copyright 2018 Arizona Board of Regents on behalf of University of Arizona.      #
+# All information, intellectual, and technical concepts contained herein is and shall       #
+# remain the proprietary information of Arizona Board of Regents and may be covered         #
+# by U.S. and Foreign Patents, and patents in process.  Dissemination of this information   #       
+# or reproduction of this material is strictly forbidden unless prior written permission    #
+# is obtained from Arizona Board of Regents or University of Arizona.                       #
+#                                                                                           #
+# lmmitss-initialize.sh                                                                     #
+# Created by Niraj Altekar                                                                  #
+# Transportation Research Institute                                                         #
+# Systems and Industrial Engineering                                                        #
+# The University of Arizona                                                                 #
+#                                                                                           #
+# This code was develop under the supervision of Professor Larry Head                       #
+# in the Transportation Research Institute.                                                 #
+#                                                                                           #
+# Revision History:                                                                         #
+# This script configures the directory structure and libraries required by the MMITSS apps. #
+# The intersection configuration files will be stored in /nojournal/bin/                    #
+# The log files for each simulation run will be stored in /nojournal/bin/log/               #
+#                                                                                           #
+#############################################################################################
+
+
+
+#*********************************************************************************************
+#THIS SCRIPT NEEDS A SUPERUSER ACCESS TO RUN, AS IT CREATES DIRECTORIES IN THE ROOT FOLDER.  *
+#*********************************************************************************************
+
+#Request the user-name, user-group, and architecture
+read -p "Username: " username
+read -p "User Group: " usergroup
+read -p "Architecture - x86 or arm: " arch
+read -p "Name of network adapter to be used by MMITSS: " mmitss_network_adapter
+read -p "Copy sample configuration file to /nojournal/bin? (y or n): " copy_sample 
+
+echo "Adding MMITSS_ROOT to ~/.bashrc"
+echo "export MMITSS_ROOT=$(pwd)/../../.." >> ~/.bashrc
+
+echo "Adding MMITSS_NETWORK_ADAPTER to ~/.bashrc"
+echo "export MMITSS_NETWORK_ADAPTER=$mmitss_network_adapter" >> ~/.bashrc
+
+echo "Adding PROCESSOR to ~/.bashrc"
+echo "export PROCESSOR=$arch" >> ~/.bashrc
+
+echo "Creating required directories in the root folder."
+sudo rm -r /nojournal/
+sudo rm -r /usr/local/lib/mmitss
+sudo mkdir -p /nojournal/bin/log
+sudo mkdir /usr/local/lib/mmitss
+sleep 1s
+
+if [ "$copy_sample" = "y" ]; then
+echo "Copy the configuration files of the intersection Daisy-Gavilan to /nojournal/bin/"
+sudo cp -r ../../config/anthem/field/daisy-gavilan/nojournal/bin /nojournal
+sleep 1s
+echo "Change the owner and group of the configuration files and provide necessary permissions (chmod 777)"
+sudo chown -R $username:$usergroup /nojournal
+sudo chmod -R 777 /nojournal
+sleep 1s
+fi
+
+echo "Add the shared libraries we need to run"
+
+if [ "$arch" = "x86" ]; then
+sudo cp ../../3rdparty/net-snmp/lib/x86/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/glpk/lib/x86/libglpk.so.35.1.0 /usr/local/lib/mmitss/
+sudo cp ../../lib/x86/libmmitss-common.so /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/mapengine/lib/x86/liblocAware.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/asn1j2735/lib/x86/libasn.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/asn1j2735/lib/x86/libdsrc.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
+echo "Create the symbolic links for the copied libraries."
+sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
+sudo ln -s /usr/local/lib/mmitss/libglpk.so.35.1.0 /usr/local/lib/mmitss/libglpk.so.35
+sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
+sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
+sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+fi
+
+if [ "$arch" = "arm" ]; then
+sudo cp ../../3rdparty/net-snmp/lib/arm/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/glpk/lib/arm/libglpk.so.40.3.0 /usr/local/lib/mmitss/
+sudo cp ../../lib/arm/libmmitss-common.so /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/mapengine/lib/arm/liblocAware.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/asn1j2735/lib/arm/libasn.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../3rdparty/asn1j2735/lib/arm/libdsrc.so.1.0 /usr/local/lib/mmitss/
+sudo cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
+sudo cp ../../3rdparty/openssl/* /usr/local/lib
+
+
+echo "Create the symbolic links for the copied libraries."
+sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
+sudo ln -s /usr/local/lib/mmitss/libglpk.so.40.3.0 /usr/local/lib/mmitss/libglpk.so.40
+sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
+sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
+sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+fi
+
+sleep 1s
+
+
+
+sudo ldconfig
+pkill -9 sleep #End
