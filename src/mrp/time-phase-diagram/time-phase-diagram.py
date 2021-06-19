bf2028fa2059d7016966b87b9c28008e5b94cd7b
debipe20 <debashisdas@email.arizona.edu>
2021-06-18 18:13:16 -0700

    synced time-phase diagram tool with solver

--- src/mrp/time-phase-diagram/time-phase-diagram.py
+++ src/mrp/time-phase-diagram/time-phase-diagram.py
@@ -2,36 +2,69 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
-import json
+import json, time
 import socket
+import os
 from OptimizationResultsManager import OptimizationResultsManager
 
 
+def createDirectory():
+    path = "/nojournal/bin/performance-measurement-diagrams/time-phase-diagram"
 
-def main():
-    # Read the config file into a json object:
+    if not os.path.exists(path + "/archive"):
+        os.makedirs(path + "/archive")
+
+def checkTimePhaseDigramGeneratingStatus(configFile):  
+    diagramGenerationStatus = False
+    
     configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
-    config = (json.load(configFile))
-   
+    config = json.load(configFile)
+
     # Close the config file:
     configFile.close()
     
+    diagramGenerationStatus = config["PerformanceMeasurementDiagram"]
+    
+    return diagramGenerationStatus
+    
+def main():
+    # Read the config file into a json object:
+    configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
+    config = json.load(configFile)
+
+    # Close the config file:
+    configFile.close()
+
     # Open a socket and bind it to the IP and port dedicated for this application:
     timePhaseDiagramSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     hostIp = config["HostIp"]
-    port = config["PortNumber"]["SignalCoordination"]
+    port = config["PortNumber"]["TimePhaseDiagramTool"]
     timePhaseDiagram_commInfo = (hostIp, port)
     timePhaseDiagramSocket.bind(timePhaseDiagram_commInfo)
+    timePhaseDiagramSocket.settimeout(2)
     
+    timeGapBetweenDiagramGenerationStatusChecking = config['SystemPerformanceTimeInterval']
+    diagramGenerationStatus = checkTimePhaseDigramGeneratingStatus(configFile)
+    generateDiagramStatusCheckingTime = time.time()
+    
+    createDirectory()
     optimizationResultsManager = OptimizationResultsManager()
-    optimizationResultsManager.readOptimizationResultsFile('/nojournal/bin/OptimizationResults.txt')
+    
 
-    # while True:
-    #     data, address = timePhaseDiagramSocket.recvfrom(10240)
-    #     data = data.decode()
-    #     receivedMessage = json.loads(data)
-    #     if receivedMessage["MsgType"]=="Schedule":
-    #         pass
-            
+    while True:
+        try:
+            data, address = timePhaseDiagramSocket.recvfrom(1024)
+            data = data.decode()
+            receivedMessage = json.loads(data)
+            if receivedMessage["MsgType"]=="TimePhaseDiagram" and bool(diagramGenerationStatus):
+                optimizationResultsManager.readOptimizationResultsFile()
+                
+        except:
+            if (time.time() - generateDiagramStatusCheckingTime) >= timeGapBetweenDiagramGenerationStatusChecking:
+                diagramGenerationStatus = checkTimePhaseDigramGeneratingStatus(configFile)
+                generateDiagramStatusCheckingTime = time.time() 
+
+    timePhaseDiagramSocket.close()
+
 if __name__ == '__main__':
     main()
